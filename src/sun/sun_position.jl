608d9d3c77049c6041ff5699144ba6ba7a0caf43
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2017-08-03 18:07:25 -0300

    :wrench: Update the algorithm to compute the Sun position
    The algorithm to compute the Sun position is now based on [1],
    which was obtained from [2]. This is an updated version of the
    previous algorithm that was based on [3].
    
    [1] Vallado, D. A (2013). Fundamentals of Astrodynamics and
        Applications.  Microcosm Press, Hawthorne, CA.
    [2] The Astronomical Almanac for the year 2006 (p. C24).
    [3] The Astronomical Almanac for the year 2000 (p. C24).

--- src/sun/sun_position.jl
+++ src/sun/sun_position.jl
@@ -17,10 +17,20 @@
 # References:
 #    [1] The Astronomical Almanac for the year 2000 (p. C24).
 #    [2] http://aa.usno.navy.mil/faq/docs/SunApprox.php
+#    [3] Vallado, D. A (2013). Fundamentals of Astrodynamics and Applications.
+#        Microcosm Press, Hawthorne, CA.
+#    [4] The Astronomical Almanac for the year 2006.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Changelog
+#
+# 2017-08-03: Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
+#    - Update the function `sun_position_i` to use the algorithm in [3], which
+#      was obtained from [4]. This is more accurate than the previous version.
+#      However, it was noted that the output is represented in the Mean Equinox
+#      of Date (MOD). Then it must be converted to the inertial system that will
+#      be used (J2000, True of Data, etc.).
 #
 # 2016-01-26: Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
 #    - The function sun_position_i was changed to receive the Julian Day. The
@@ -80,6 +90,7 @@ end
 ### function sun_position_i(day::Int, time::Int)
 
 Compute the Sun position represented in the Inertial coordinate frame (J2000).
+The algorithm was adapted from [3, p. 277-279].
 
 ##### Args
 
@@ -96,55 +107,35 @@ function sun_position_i(JD::Real)
     # Constants
     const deg2rad = pi/180
 
-    # Compute the days from the noon of 1 January, 2000, which is the input to
-    # the algorithm.
-    D = JD - JD_J2000
-
-    # # # # # # # # # # # # # # # #
-    # Ecliptic coordinates
-    # # # # # # # # # # # # # # # #
-
-    # Mean anomaly of the Sun [rad].
-    g = mod(357.529 + 0.98560028*D, 360)
-    g = g*deg2rad
+    # Number of Julian centuries from J2000 epoch.
+    T_UT1 = (JD-JD_J2000)/36525.0
 
     # Mean longitude of the Sun [deg].
-    q = mod(280.459 + 0.98564736*D, 360)
+    λ_m = mod(280.460 + 36000.771*T_UT1, 360.0)
 
-    # Geocentric apparent ecliptic longitude of the Sun (adjusted for
-    # aberration) [rad].
-    L = mod(q + 1.915*sin(g) + 0.020*sin(2g), 360)
-    L = L*deg2rad
+    # Mean anomaly of the Sun [deg].
+    #
+    # Here, we should use T_TBD (Barycentric Dynamical Time). However, it is
+    # sufficient to use T_UT1 because this is a low precision computation [1].
+    Ms = mod(357.5291092 + 35999.05034*T_UT1, 360.0)
+
+    # Ecliptic latitude of the Sun [deg].
+    λ_ecliptic = mod(λ_m + 1.914666471*sin(Ms*deg2rad) +
+                           0.019994643*sin(2*Ms*deg2rad), 360.0)
+
+    # Obliquity of the ecliptic [deg].
+    #
+    # Here, we are also approximating T_TBD ≈ T_UT1.
+    ϵ = mod(23.439291 - 0.0130042*T_UT1, 360.0)
 
     # Distance of the Sun from Earth [m].
-    R = (1.00014 - 0.01671*cos(g) - 0.00014*cos(2g))*au2m
+    r = (1.000140612 - 0.016708617*cos(Ms) - 0.000139589*cos(2*Ms))*au2m
 
-    # Obliquity of ecliptic [rad].
-    e = (23.439 - 0.00000036*D)*deg2rad
+    # Compute the Sun vector represented in the Mean Equinox of Date (MOD).
+    S_MOD = [r*cos(λ_ecliptic*deg2rad);
+             r*cos(ϵ*deg2rad)*sin(λ_ecliptic*deg2rad);
+             r*sin(ϵ*deg2rad)*sin(λ_ecliptic*deg2rad);]
 
-    # # # # # # # # # # # # # # # #
-    # Inertial (Celestial) coordinates
-    # # # # # # # # # # # # # # # #
-
-    # Right ascension and declination.
-    cos_L = cos(L)
-    sin_L = sin(L)
-
-    cos_e = cos(e)
-    sin_e = sin(e)
-
-    ra_i = atan2( cos_e*sin_L, cos_L )
-    dec_i = asin( sin_e*sin_L )
-
-    # Compute the vector from the origin of the Celestial Coordinate Frame to
-    # the center of the Sun represented in the Celestial Coordinate Frame.
-    cos_ra_i = cos(ra_i)
-    sin_ra_i = sin(ra_i)
-
-    cos_dec_i = cos(dec_i)
-    sin_dec_i = sin(dec_i)
-
-    S_i = [cos_ra_i*cos_dec_i;
-           sin_ra_i*cos_dec_i;
-           sin_dec_i]*R
+    # TODO: This vector must be transformed to the inertial reference frame that
+    # will be used in this toolbox (J2000, True of Date, etc.).
 end
