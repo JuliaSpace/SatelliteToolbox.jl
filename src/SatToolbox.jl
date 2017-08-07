541dae11549a0bf9ae2c8e63c413d9a17d9e1d10
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2017-08-07 09:50:57 -0300

    :package: Create a file for constants
    All the constants were moved to the file `constants.jl`.
    Furthermore, the gravitational parameters were updated based on
    EGM-08.

--- src/SatToolbox.jl
+++ src/SatToolbox.jl
@@ -15,39 +15,7 @@ import Base: asin, atan2, cos, mod, sin
 #                                  Constants
 ################################################################################
 
-# Julian Day of J2000.0 epoch.
-const JD_J2000 = 2451545.0
-
-# Earth Equatorial radius [m].
-const R0 = 6378137.0;
-
-# Earth mean radius [m].
-const Rm = 6371009.0;
-
-# Standard gravitational parameter for Earth [m^3/s^2]
-const m0 = 3.986004415e14;
-
-# J2 perturbation term.
-const J2 = 0.0010826267
-
-# Sun radius [m].
-const Rs = 6.963e8
-
-# Earth's orbit mean motion [rad/s]
-const ne = (360.0/365.2421897)*pi/180/86400
-
-# Conversion factor from AU to m.
-const au2m = 149597870700.0
-
-# Sun radiation emitted [J/sec].
-const sunRad = 3.826e26
-
-# WGS-84 Data.
-const a_wgs84  = 6378137.0
-const f_wgs84  = 1/298.257223563
-const b_wgs84  = a_wgs84*(1-f_wgs84)
-const e_wgs84  = sqrt( (a_wgs84^2-b_wgs84^2)/a_wgs84^2 )
-const el_wgs84 = sqrt( (a_wgs84^2-b_wgs84^2)/b_wgs84^2 )
+include("constants.jl")
 
 ################################################################################
 #                                  Exceptions
