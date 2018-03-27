4535cdbbe4ba1f1960cb49c5ad36a0d4d3e2bc7f
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-03-27 18:00:33 -0300

    :sparkles: Add structure to store the orbit
    The structure `Orbit` was created to store the satellite orbit. All
    the general functions were updated to support this structure.

--- src/orbit/general.jl
+++ src/orbit/general.jl
@@ -25,7 +25,60 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ==#
 
+export Orbit
 export angvel, dArgPer, dRAAN, period
+
+################################################################################
+#                                    Types
+################################################################################
+
+"""
+This structure defines the orbit in terms of the Keplerian elements.
+"""
+mutable struct Orbit
+    t::Number  # Orbit epoch.
+    a::Number  # Semi-major axis [m].
+    e::Number  # Eccentricity.
+    i::Number  # Inclination [rad].
+    Ω::Number  # Right ascension of the ascending node [rad].
+    ω::Number  # Argument of perigee [rad].
+    f::Number  # True anomaly [rad].
+end
+
+################################################################################
+#                                  Functions
+################################################################################
+
+"""
+### function Orbit(a::Number, e::Number, i::Number, Ω::Number, ω::Number, f::Number)
+
+Create an orbit with semi-major axis `a`, eccentricity `e`, inclination `i`,
+right ascension of the ascending node `Ω`, argument of perigee `ω`, and true
+anomaly `f`.
+
+##### Args
+
+* a: Semi-major axis [m].
+* e: Eccentricity.
+* i: Inclination [rad].
+* Ω: Right ascension of the ascending node [rad].
+* ω: Argument of perigee [rad].
+* f: True anomaly [rad].
+
+##### Returns
+
+An object of type `Orbit` with the specified orbit. The orbit epoch is defined
+as 0.0.
+
+"""
+
+function Orbit(a::Number, e::Number, i::Number, Ω::Number, ω::Number, f::Number)
+    Orbit(0.0, a, e, i, Ω, ω, f)
+end
+
+################################################################################
+#                                  Functions
+################################################################################
 
 """
 ### function angvel(a::Number, e::Number, i::Number, pert::Symbol = :J2)
@@ -79,6 +132,32 @@ function angvel(a::Number, e::Number, i::Number, pert::Symbol = :J2)
 end
 
 """
+### function angvel(orb::Orbit, pert::Symbol = :J2)
+
+Compute the angular velocity of an object in an orbit `orb` using the
+perturbation terms specified by the symbol `pert`.
+
+`pert` can be:
+
+* `:J0`: Consider a Keplerian orbit.
+* `:J2`: Consider the perturbation terms up to J2.
+
+##### Args
+
+* orb: Orbit (see `Orbit`).
+* pert: (OPTIONAL) Symbol that defines the perturbation (**DEFAULT** = `:J2`).
+
+##### Returns
+
+The angular velocity of an object in the specified orbit [rad/s].
+
+"""
+
+function angvel(orb::Orbit, pert::Symbol = :J2)
+    angvel(orb.a, orb.e, orb.i, pert)
+end
+
+"""
 ### function dArgPer(a::Number, e::Number, i::Number, pert::Symbol = :J2)
 
 Compute the time-derivative of the argument of perigee of an orbit with
@@ -129,6 +208,32 @@ function dArgPer(a::Number, e::Number, i::Number, pert::Symbol = :J2)
 end
 
 """
+### function dArgPer(orb::Orbit, pert::Symbol = :J2)
+
+Compute the time-derivative of the argument of perigee of an orbit `orb` using
+the perturbation terms specified by the symbol `pert`.
+
+`pert` can be:
+
+* `:J0`: Consider a Keplerian orbit.
+* `:J2`: Consider the perturbation terms up to J2.
+
+##### Args
+
+* orb: Orbit (see `Orbit`).
+* pert: (OPTIONAL) Symbol that defines the perturbation (**DEFAULT** = `:J2`).
+
+##### Returns
+
+The perturbation of the argument of perigee [rad/s].
+
+"""
+
+function dArgPer(orb::Orbit, pert::Symbol = :J2)
+    dArgPer(org.a, org.e, orb.i, pert)
+end
+
+"""
 ### function dRAAN(a::Number, e::Number, i::Number, pert::Symbol = :J2)
 
 Compute the time-derivative of the right ascension of the ascending node of an
@@ -175,6 +280,32 @@ function dRAAN(a::Number, e::Number, i::Number, pert::Symbol = :J2)
     else
         throw(ArgumentError("The perturbation parameter $pert is not defined."))
     end
+end
+
+"""
+### function dRAAN(orb::Orbit, pert::Symbol = :J2)
+
+Compute the time-derivative of the right ascension of the ascending node of an
+orbit `orb` using the perturbation terms specified by the symbol `pert`.
+
+`pert` can be:
+
+* `:J0`: Consider a Keplerian orbit.
+* `:J2`: Consider the perturbation terms up to J2.
+
+##### Args
+
+* orb: Orbit (see `Orbit`).
+* pert: Symbol that defines the perturbation (DEFAULT = `:J2`).
+
+##### Returns
+
+The time derivative of the RAAN [rad/s].
+
+"""
+
+function dRAAN(orb::Orbit, pert::Symbol = :J2)
+    dRAAN(orb.a, orb.e, orb.i, pert)
 end
 
 
@@ -237,4 +368,30 @@ The orbit period [s].
 function period(a::Number, e::Number, i::Number, pert::Symbol = :J2)
     n = angvel(a, e, i, pert)
     2.0*pi/n
+end
+
+"""
+### function period(orb::Orbit, pert::Symbol = :J2)
+
+Compute the period of an object in an orbit `orb` using the perturbation terms
+specified by the symbol `pert`.
+
+`pert` can be:
+
+* `:J0`: Consider a Keplerian orbit.
+* `:J2`: Consider the perturbation terms up to J2.
+
+##### Args
+
+* orb: Orbit (see `Orbit`).
+* pert: (OPTIONAL) Symbol that defines the perturbation (**DEFAULT** = `:J2`).
+
+##### Returns
+
+The orbit period [s].
+
+"""
+
+function period(orb::Orbit, pert::Symbol = :J2)
+    period(orb.a, orb.e, orb.i, pert)
 end
