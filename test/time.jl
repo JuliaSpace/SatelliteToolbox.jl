353343f6e7c5efcaa6c4ad6666d7e0f14933f07a
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-04-28 21:10:23 -0300

    :rotating_light: Add tests for the function get_ΔAT

--- test/time.jl
+++ test/time.jl
@@ -17,6 +17,7 @@
 # References
 #
 #   [1] http://aa.usno.navy.mil/data/docs/JulianDate.php
+#   [2] https://www.ietf.org/timezones/data/leap-seconds.list
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
@@ -26,6 +27,71 @@
 #   Initial version.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ==#
+
+# File: ./src/time/time.jl
+# ========================
+
+println("$(c)Testing functions in file: ./src/time/time.jl$d")
+println("$(c)---------------------------------------------$d")
+println("")
+
+println("    Testing function get_ΔAT...")
+
+# Leap seconds values obtained from [2].
+leap_secs = [2272060800	10	1 "Jan" 1972;
+             2303683200	12	1 "Jan" 1973;
+             2287785600	11	1 "Jul" 1972;
+             2335219200	13	1 "Jan" 1974;
+             2366755200	14	1 "Jan" 1975;
+             2398291200	15	1 "Jan" 1976;
+             2429913600	16	1 "Jan" 1977;
+             2461449600	17	1 "Jan" 1978;
+             2492985600	18	1 "Jan" 1979;
+             2524521600	19	1 "Jan" 1980;
+             2571782400	20	1 "Jul" 1981;
+             2603318400	21	1 "Jul" 1982;
+             2634854400	22	1 "Jul" 1983;
+             2698012800	23	1 "Jul" 1985;
+             2776982400	24	1 "Jan" 1988;
+             2840140800	25	1 "Jan" 1990;
+             2871676800	26	1 "Jan" 1991;
+             2918937600	27	1 "Jul" 1992;
+             2950473600	28	1 "Jul" 1993;
+             2982009600	29	1 "Jul" 1994;
+             3029443200	30	1 "Jan" 1996;
+             3076704000	31	1 "Jul" 1997;
+             3124137600	32	1 "Jan" 1999;
+             3345062400	33	1 "Jan" 2006;
+             3439756800	34	1 "Jan" 2009;
+             3550089600	35	1 "Jul" 2012;
+             3644697600	36	1 "Jul" 2015;
+             3692217600	37	1 "Jan" 2017;
+            ]
+
+for i = 1:size(leap_secs,1)
+    ΔAT   = leap_secs[i,2]
+    day   = leap_secs[i,3]
+    month = begin
+        if leap_secs[i,4] == "Jan"
+            1
+        elseif leap_secs[i,4] == "Jul"
+            7
+        else
+            error("Invalid array `leap_secs`.")
+        end
+    end
+    year  = leap_secs[i,5]
+
+    @test get_ΔAT(DatetoJD(year,month,day,0,0,0))         == ΔAT
+
+    # One second before the entry in `leap_secs`, we must have `ΔAT-1` leap
+    # seconds. However, this is not true for the very first line.
+    (i == 1) && continue
+    @test get_ΔAT(DatetoJD(year,month,day,0,0,0)-1/86400) == ΔAT-1
+end
+
+println("        $(b)Test passed!$d")
+println("")
 
 # File: ./src/time/julian_day.jl
 # ==============================
