8acd38ccf107236c45418601b2bc978025a31b1f
ronisbr <ronisbr@gmail.com>
2018-05-06 11:48:22 -0300

    :sparkles: Add conventional Legendre associated functions
    The functions to compute the conventional Legendre associated
    functions were added.

--- src/aux/legendre.jl
+++ src/aux/legendre.jl
@@ -36,6 +36,10 @@
 #
 # Changelog
 #
+# 2018-05-06: Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
+#   - Add the functions to compute the conventional Schmidt associated Legendre
+#     functions.
+#
 # 2018-04-30: Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
 #   - Improve the structure of the source code.
 #   - Add function to compute the Schmidt quasi-normalized associated Legendre
@@ -52,33 +56,7 @@ export denormalize_legendre, legendre!, legendre
 
 export legendre_fully_normalized!, legendre_fully_normalized
 export legendre_schmidt_quasi_normalized!, legendre_schmidt_quasi_normalized
-
-"""
-### function denormalize_legendre(P_lm::Number, n::Number, m::Number)
-
-Compute the conventional associated Legendre function from a fully normalized
-value `P_nm` that has order `n` and degree `m`.
-
-##### Args
-
-* P_nm: Fully normalized Legendre associated function with degree `n` and order
-        `m`.
-* n: Degree of `P_nm`.
-* m: Order of `P_nm`.
-
-##### Returns
-
-The conventional associated Legendre function.
-
-"""
-
-function denormalize_legendre(P_nm::Number, n::Number, m::Number)
-    (n-m < 0) && throw(ArgumentError("n must be equal or bigger than m."))
-
-    k = (m == 0) ? 1 : 2
-
-    P_nm/sqrt(k*(2n+1)*factorial(n-m)/factorial(n+m))
-end
+export legendre_conventional!, legendre_conventional
 
 """
 ### function legendre!([N,] P::Matrix, ϕ::Number, ph_term::Bool = false)
@@ -96,6 +74,8 @@ following values are valid:
   `legendre_fully_normalized!`).
 * `Val{:schmidt}`: Compute the Schmidt quasi-normalized associated Legendre
   function (see `legendre_schmidt_quasi_normalized!`).
+* `Val{:conv}`: Compute the conventional associated Legendre function (see
+  `legendre_conventional!`).
 
 If `N` is omitted, then the full normalization will be used.
 
@@ -117,6 +97,9 @@ legendre!(::Type{Val{:full}}, P::Matrix, ϕ::Number, ph_term::Bool = false) =
 legendre!(::Type{Val{:schmidt}}, P::Matrix, ϕ::Number, ph_term::Bool = false) =
     legendre_schmidt_quasi_normalized!(P, ϕ, ph_term)
 
+legendre!(::Type{Val{:conv}}, P::Matrix, ϕ::Number, ph_term::Bool = false) =
+    legendre_conventional!(P, ϕ, ph_term)
+
 """
 ### function legendre([N,] ϕ::Number, n_max::Number, ph_term::Bool = false)
 
@@ -130,6 +113,8 @@ following values are valid:
   `legendre_fully_normalized!`).
 * `Val{:schmidt}`: Compute the Schmidt quasi-normalized associated Legendre
   function (see `legendre_schmidt_quasi_normalized!`).
+* `Val{:conv}`: Compute the conventional associated Legendre function (see
+  `legendre_conventional!`).
 
 If `N` is omitted, then the full normalization will be used.
 
@@ -395,5 +380,96 @@ function legendre_schmidt_quasi_normalized(ϕ::Number,
 
     P = zeros(n_max+1, n_max+1)
     legendre_schmidt_quasi_normalized!(P, ϕ, ph_term)
+    P
+end
+
+################################################################################
+#                  Conventional Associated Legendre Function
+################################################################################
+
+"""
+### function legendre_conventional!(P::Matrix, ϕ::Number, ph_term::Bool = false)
+
+Compute the conventional associated Legendre function `P_n,m[cos(ϕ)]`.
+The maximum degree and order that will be computed are given by the dimensions
+of matrix `P`. Notice, however, that `P` must be a square matrix.
+
+The result will be stored at matrix `P`.
+
+##### Args
+
+* P: Matrix that will store the computed associated Legendre function.
+* ϕ: Angle [rad].
+* ph_term: (OPTIONAL) Include the Condon-Shortley phase term `(-1)^m`
+           (**DEFAULT** = `false`).
+
+"""
+
+function legendre_conventional!(P::Matrix,
+                                ϕ::Number,
+                                ph_term::Bool = false)
+    (rows, cols) = size(P)
+
+    # Check if the matrix P is a square matrix.
+    (rows != cols) && throw(ArgumentError("P must be a square matrix."))
+
+    # The matrix must be, at least, 2 rows and 2 columns.
+    (rows < 2) && throw(ArgumentError("P must have at least 2 rows."))
+
+    # Auxiliary variables to improve code performance.
+    s = sin(ϕ)
+    c = cos(ϕ)
+
+    # Starting values.
+    P[0+1,0+1] = 1
+    P[1+1,0+1] = +c
+    P[1+1,1+1] = -s
+
+    (ph_term) && (P[1+1,1+1] *= -1)
+
+    for n = 2:rows-1
+        for m = 0:n-1
+            aux = (n-m)*(n-m)
+            a_nm = sqrt( ( (2n-1)*(2n-1) ) / aux )
+            b_nm = sqrt( ( (n+m-1)*(n-m-1) ) / aux )
+            P[n+1,m+1] = a_nm*c*P[n-1+1,m+1] - b_nm*P[n-2+1,m+1]
+        end
+
+        if ph_term
+            P[n+1,n+1] = +s*sqrt( (2n-1)*(2n-1) )*P[n-1+1,n-1+1]
+        else
+            P[n+1,n+1] = -s*sqrt( (2n-1)*(2n-1) )*P[n-1+1,n-1+1]
+        end
+    end
+
+    nothing
+end
+
+"""
+### function legendre_conventional(ϕ::Number, n_max::Number, ph_term::Bool = false)
+
+Compute the conventional associated Legendre function `P_n,m[cos(ϕ)]`.
+The maximum degree that will be computed is `n_max`.
+
+##### Args
+
+* ϕ: Angle [rad].
+* n_max: The maximum degree that will be computed.
+* ph_term: (OPTIONAL) Include the Condon-Shortley phase term `(-1)^m`
+           (**DEFAULT** = `false`).
+
+##### Returns
+
+A square matrix with the Legendre associated functions `P_n,m[cos(ϕ)]`.
+
+"""
+
+function legendre_conventional(ϕ::Number,
+                               n_max::Number,
+                               ph_term::Bool = false)
+    (n_max < 1) && throw(ArgumentError("n_max must be at least 1."))
+
+    P = zeros(n_max+1, n_max+1)
+    legendre_conventional!(P, ϕ, ph_term)
     P
 end
