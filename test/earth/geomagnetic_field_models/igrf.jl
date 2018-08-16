aff2c5306b6bcfe63091f3d86af9ca9aac8f8297
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-08-16 15:21:56 -0300

    :rotating_light: Improve IGRF tests
    The IGRF test data is now obtained using IGRF_Test@0f30804. In this
    case, it is considered the dates between 2020 and 2025.
    Furthermore, it is also added the tests related to the warning
    messages.

--- test/earth/geomagnetic_field_models/igrf.jl
+++ test/earth/geomagnetic_field_models/igrf.jl
@@ -36,7 +36,13 @@ igrf12_geodetic_test   = readdlm("./IGRF12_test_geodetic.txt")
         ft    = igrf12_geocentric_test[i,8]
 
         # Call IGRF with the same inputs as those in the test.
-        (x, y, z, f) = igrf12syn(0, date, 2, r, colat, elong)
+
+        if date < 2020
+            (x, y, z, f) = igrf12syn(0, date, 2, r, colat, elong)
+        else
+            (x, y, z, f) = @test_logs (:warn, "The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than 2020.") #=
+                =# igrf12syn(0, date, 2, r, colat, elong)
+        end
 
         # Test the values.
         @test x ≈ xt atol=3e-1
@@ -57,7 +63,12 @@ igrf12_geodetic_test   = readdlm("./IGRF12_test_geodetic.txt")
         ft    = igrf12_geodetic_test[i,8]
 
         # Call IGRF with the same inputs as those in the test.
-        (x, y, z, f) = igrf12syn(0, date, 1, h, colat, elong)
+        if date < 2020
+            (x, y, z, f) = igrf12syn(0, date, 1, h, colat, elong)
+        else
+            (x, y, z, f) = @test_logs (:warn, "The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than 2020.") #=
+                =# igrf12syn(0, date, 1, h, colat, elong)
+        end
 
         # Test the values.
         @test x ≈ xt atol=3e-1
@@ -84,7 +95,13 @@ end
 
         # Call IGRF with the same inputs as those in the test.
         (elong > 180) && (elong = elong-360)
-        B = igrf12(date, r*1000, (90-colat)*pi/180, elong*pi/180)
+
+        if date < 2020
+            B = igrf12(date, r*1000, (90-colat)*pi/180, elong*pi/180)
+        else
+            B = @test_logs (:warn, "The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than 2020.") #=
+                =# igrf12(date, r*1000, (90-colat)*pi/180, elong*pi/180)
+        end
 
         x, y, z = B[:]
         f       = norm(B)
@@ -109,7 +126,13 @@ end
 
         # Call IGRF with the same inputs as those in the test.
         (elong > 180) && (elong = elong-360)
-        B = igrf12(date, h*1000, (90-colat)*pi/180, elong*pi/180, Val{:geodetic})
+
+        if date < 2020
+            B = igrf12(date, h*1000, (90-colat)*pi/180, elong*pi/180, Val{:geodetic})
+        else
+            B = @test_logs (:warn, "The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than 2020.") #=
+                =# igrf12(date, h*1000, (90-colat)*pi/180, elong*pi/180, Val{:geodetic})
+        end
 
         x, y, z = B[:]
         f       = norm(B)
@@ -121,4 +144,3 @@ end
         @test f ≈ ft atol=3e-1
     end
 end
-
