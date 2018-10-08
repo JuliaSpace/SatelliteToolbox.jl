8e4f88ec2e38e231f4869fe7db6da3b0111b6a2e
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-10-08 15:45:57 -0300

    :wrench: `propagate!` can now receive one timestamp
    The `propagate!` API can now receive a single time to propagate the
    orbit.
    
    Closes #11

--- src/orbit/propagators/api/propagate.jl
+++ src/orbit/propagators/api/propagate.jl
@@ -9,26 +9,32 @@
 export propagate!
 
 """
+    function propagate!(orbp, t::Number) where T
     function propagate!(orbp, t::AbstractVector) where T
 
-Propagate the orbit in `orbp` using the time instants defined in the vector `t`
-using the algorithm related to the structure `orbp`. The structure `orbp` will
-contain the elements at the last propagation instant.
+If `t` is a number, then propagate `orbp` until the epoch defined by `t`.
+Otherwise, if `t` is an array, then propagate the orbit in `orbp` using the time
+instants defined in the vector `t`.
+
+In both cases, the orbit propagator algorithm is the one related to the
+structure `orbp`.
+
+The structure `orbp` will contain the elements at the last propagation instant.
 
 # Args
 
 * `orbp`: Propagator structure.
-* `t`: Time instants from orbit epoch in which the orbit will be propagated
-       [s].
+* `t`: Time instant(s) in which the orbit will be propagated [s].
 
 # Returns
 
-* An array with the mean Keplerian elements represented in inertial frame in
-  each time instant (see `Orbit`) [SI units].
-* An array with the position vector represented in inertial frame in each time
-  instant [m].
-* An array with the velocity vector represented in inertial frame in each time
-  instant [m].
+* The mean Keplerian elements represented in inertial frame in each time instant
+  (see `Orbit`) [SI units].
+* The position vector represented in inertial frame in each time instant [m].
+* The velocity vector represented in inertial frame in each time instant [m].
+
+If `t` is an array, then those values will be an array containing the
+information related to each epoch in `t`.
 
 # Remarks
 
@@ -38,94 +44,85 @@ from a TLE, then the inertial frame will be TEME. Notice, however, that the
 perturbation theory requires an inertial frame with true equator.
 
 """
-function propagate!(orbp::OrbitPropagatorJ2{T}, t::AbstractVector) where T
+function propagate!(orbp::OrbitPropagatorJ2{T}, t::Number) where T
     # Auxiliary variables.
     orb = orbp.orb
     j2d = orbp.j2d
 
-    # Output.
-    result_orb = Array{Orbit{T,T,T,T,T,T,T}}(undef, 0)
-    result_r   = Array{Vector{T}}(undef, 0)
-    result_v   = Array{Vector{T}}(undef, 0)
+    # Propagate the orbit.
+    (r_i, v_i) = j2!(j2d, t)
 
-    for k in t
-        # Propagate the orbit.
-        (r_i_k, v_i_k) = j2!(j2d, k)
+    # Update the elements in the `orb` structure.
+    orb.t = j2d.epoch + t/86400
+    orb.a = j2d.a_k*j2d.j2_gc.R0
+    orb.e = j2d.e_k
+    orb.i = j2d.i_k
+    orb.Ω = j2d.Ω_k
+    orb.ω = j2d.ω_k
+    orb.f = j2d.f_k
 
-        # Update the elements in the `orb` structure.
-        orb.t = j2d.epoch + k/86400
-        orb.a = j2d.a_k*j2d.j2_gc.R0
-        orb.e = j2d.e_k
-        orb.i = j2d.i_k
-        orb.Ω = j2d.Ω_k
-        orb.ω = j2d.ω_k
-        orb.f = j2d.f_k
-
-        push!(result_orb, copy(orb))
-        push!(result_r,   r_i_k)
-        push!(result_v,   v_i_k)
-    end
-
-    (result_orb, result_r, result_v)
+    # Return.
+    (orb, r_i, v_i)
 end
 
-function propagate!(orbp::OrbitPropagatorSGP4{T}, t::AbstractVector) where T
+function propagate!(orbp::OrbitPropagatorSGP4{T}, t::Number) where T
     # Auxiliary variables.
     orb     = orbp.orb
     sgp4d   = orbp.sgp4d
     sgp4_gc = orbp.sgp4_gc
 
-    # Output.
-    result_orb = Array{Orbit{T,T,T,T,T,T,T}}(undef, 0)
-    result_r   = Array{Vector{T}}(undef, 0)
-    result_v   = Array{Vector{T}}(undef, 0)
+    # Propagate the orbit.
+    (r_teme, v_teme) = sgp4!(sgp4d, t/60)
 
-    for k in t
-        # Propagate the orbit.
-        (r_teme_k, v_teme_k) = sgp4!(sgp4d, k/60)
+    # Convert km to m.
+    r_teme *= 1000
+    v_teme *= 1000
 
-        # Convert km to m.
-        r_teme_k *= 1000
-        v_teme_k *= 1000
+    # Update the elements in the `orb` structure.
+    orb.t = sgp4d.epoch + t/86400
+    orb.a = sgp4d.a_k*sgp4_gc.R0*1000
+    orb.e = sgp4d.e_k
+    orb.i = sgp4d.i_k
+    orb.Ω = sgp4d.Ω_k
+    orb.ω = sgp4d.ω_k
+    orb.f = M_to_f(sgp4d.e_k, sgp4d.M_k)
 
-        # Update the elements in the `orb` structure.
-        orb.t = sgp4d.epoch + k/86400
-        orb.a = sgp4d.a_k*sgp4_gc.R0*1000
-        orb.e = sgp4d.e_k
-        orb.i = sgp4d.i_k
-        orb.Ω = sgp4d.Ω_k
-        orb.ω = sgp4d.ω_k
-        orb.f = M_to_f(sgp4d.e_k, sgp4d.M_k)
-
-        push!(result_orb, copy(orb))
-        push!(result_r,   r_teme_k)
-        push!(result_v,   v_teme_k)
-    end
-
-    (result_orb, result_r, result_v)
+    # Return.
+    (orb, r_teme, v_teme)
 end
 
-function propagate!(orbp::OrbitPropagatorTwoBody{T}, t::AbstractVector) where T
+function propagate!(orbp::OrbitPropagatorTwoBody{T}, t::Number) where T
     # Auxiliary variables.
     orb = orbp.orb
     tbd = orbp.tbd
 
+    # Propagate the orbit.
+    (r_i, v_i) = twobody!(tbd, t)
+
+    # Update the elements in the `orb` structure.
+    orb.t = tbd.epoch + t/86400
+    orb.f = tbd.f_k
+
+    (orb, r_i, v_i)
+end
+
+function propagate!(orbp::Union{OrbitPropagatorJ2{T},
+                                OrbitPropagatorSGP4{T},
+                                OrbitPropagatorTwoBody{T}},
+                    t::AbstractVector) where T
     # Output.
-    result_orb = Array{Orbit{T,T,T,T,T,T,T}}(undef, 0)
-    result_r   = Array{Vector{T}}(undef, 0)
-    result_v   = Array{Vector{T}}(undef, 0)
+    num_elems  = length(t)
+    result_orb = Array{Orbit{T,T,T,T,T,T,T}}(undef, num_elems)
+    result_r   = Array{Vector{T}}(undef, num_elems)
+    result_v   = Array{Vector{T}}(undef, num_elems)
 
-    for k in t
+    for k = 1:num_elems
         # Propagate the orbit.
-        (r_i_k, v_i_k) = twobody!(tbd, k)
+        (orb, r_i_k, v_i_k) = propagate!(orbp, t[k])
 
-        # Update the elements in the `orb` structure.
-        orb.t = tbd.epoch + k/86400
-        orb.f = tbd.f_k
-
-        push!(result_orb, copy(orb))
-        push!(result_r,   r_i_k)
-        push!(result_v,   v_i_k)
+        result_orb[k] = orb
+        result_r[k]   = r_i_k
+        result_v[k]   = v_i_k
     end
 
     (result_orb, result_r, result_v)
