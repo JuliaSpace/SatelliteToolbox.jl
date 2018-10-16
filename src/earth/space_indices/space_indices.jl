9bb4547d9d3f037c01bee00df854823a7c2f8234
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-10-16 12:21:13 -0300

    :wrench: Allow offline files for space indices
    The user can now specify offline locations to load the space
    indices.

--- src/earth/space_indices/space_indices.jl
+++ src/earth/space_indices/space_indices.jl
@@ -95,7 +95,7 @@ dtcfile = @RemoteFile(
 ################################################################################
 
 """
-    function init_space_indices(;force_download = false)
+    function init_space_indices(;force_download = false, dtcfile_path = nothing, solfsmy_path = nothing)
 
 Initialize all space indices. The initialization process is composed of:
 
@@ -106,15 +106,30 @@ Initialize all space indices. The initialization process is composed of:
 If the keyword `force_download` is `true`, then the files will always be
 downloaded.
 
+The user can also specify the location for each required file to retrieve the
+space indices. In this case, the download will not be performed. The following
+keywords can be used for this:
+
+* `dtcfile_path`: Path to `DTCFILE.TXT`.
+* `solfsmy_path`: Path to `SOLFSMY.TXT`.
+
 """
-function init_space_indices(;force_download = false)
-    # Update the remote files.
-    download(dtcfile; force = force_download)
-    download(solfsmy; force = force_download)
+function init_space_indices(;force_download = false, dtcfile_path = nothing,
+                             solfsmy_path = nothing)
+    # Update the remote files if no path is given.
+    if dtcfile_path == nothing
+        download(dtcfile; force = force_download)
+        dtcfile_path = path(dtcfile)
+    end
+
+    if solfsmy_path == nothing
+        download(solfsmy; force = force_download)
+        solfsmy_path = path(solfsmy)
+    end
 
     # Parse each remote file.
-    push!(dtcfile_data, _parse_dtcfile(path(dtcfile)))
-    push!(solfsmy_data, _parse_solfsmy(path(solfsmy)))
+    push!(dtcfile_data, _parse_dtcfile(dtcfile_path))
+    push!(solfsmy_data, _parse_solfsmy(solfsmy_path))
 
     nothing
 end
