56a7202ade77392a38bef49d5e4dffb86d934cc4
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-11-20 13:11:57 -0200

    :lipstick: Improve Sun position algorithm

--- src/sun/sun_position.jl
+++ src/sun/sun_position.jl
@@ -29,9 +29,6 @@ Julian Day `JD`. The algorithm was adapted from [3, p. 277-279].
 
 """
 function sun_position_i(JD::Number)
-    # Constants
-    deg2rad = pi/180
-
     # Number of Julian centuries from J2000 epoch.
     T_UT1 = (JD-JD_J2000)/36525.0
 
@@ -44,9 +41,11 @@ function sun_position_i(JD::Number)
     # sufficient to use T_UT1 because this is a low precision computation [1].
     Ms = mod(357.5291092 + 35999.05034*T_UT1, 360.0)
 
+    # Convert Ms to [rad].
+    Ms = deg2rad(Ms)
+
     # Ecliptic latitude of the Sun [deg].
-    λ_ecliptic = mod(λ_m + 1.914666471*sin(Ms*deg2rad) +
-                           0.019994643*sin(2*Ms*deg2rad), 360.0)
+    λ_e = mod(λ_m + 1.914666471*sin(Ms) + 0.019994643*sin(2Ms), 360.0)
 
     # Obliquity of the ecliptic [deg].
     #
@@ -54,18 +53,18 @@ function sun_position_i(JD::Number)
     ϵ = mod(23.439291 - 0.0130042*T_UT1, 360.0)
 
     # Auxiliary variables.
-    sin_Ms         , cos_Ms         = sincos(Ms*deg2rad)
-    sin_2Ms        , cos_2Ms        = sincos(2Ms*deg2rad)
-    sin_ϵ          , cos_ϵ          = sincos(ϵ*deg2rad)
-    sin_λ_ecliptic , cos_λ_ecliptic = sincos(λ_ecliptic*deg2rad)
+    sin_Ms  , cos_Ms  = sincos(Ms)
+    sin_2Ms , cos_2Ms = sincos(2Ms)
+    sin_ϵ   , cos_ϵ   = sincos(deg2rad(ϵ))
+    sin_λ_e , cos_λ_e = sincos(deg2rad(λ_e))
 
     # Distance of the Sun from Earth [m].
     r = ( 1.000140612 - 0.016708617cos_Ms - 0.000139589cos_2Ms )*au2m
 
     # Compute the Sun vector represented in the Mean Equinox of Date (MOD).
-    S_MOD = [      r*cos_λ_ecliptic;
-             r*cos_ϵ*sin_λ_ecliptic;
-             r*sin_ϵ*sin_λ_ecliptic;]
+    S_MOD = [      r*cos_λ_e;
+             r*cos_ϵ*sin_λ_e;
+             r*sin_ϵ*sin_λ_e;]
 
     # TODO: This vector must be transformed to the inertial reference frame that
     # will be used in this toolbox (J2000, True of Date, etc.).
