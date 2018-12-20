99ba68bebe3b23c2528571834a18683ff987ae8b
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2018-12-20 13:33:04 -0200

    :race_horse: Improve the function compute_ss_orbit_by_ang_vel
    By removing the inner functions and explicit computing `f1` and
    `f2` inside the loop, it was possible to reduce the computational
    time by 88%.

--- src/orbit/orbit_sun_sync.jl
+++ src/orbit/orbit_sun_sync.jl
@@ -26,13 +26,8 @@ eccentricity `e`.
 """
 function compute_ss_orbit_by_ang_vel(n::Number, e::Number)
     # Check if the arguments are valid.
-    if (n <= 0)
-        throw(ArgumentError("The angular velocity must be greater than 0."))
-    end
-
-    if !( 0. <= e < 1. )
-        throw(ArgumentError("The eccentricity must be within the interval 0 <= e < 1."))
-    end
+    (n <= 0)          && throw(ArgumentError("The angular velocity must be greater than 0."))
+    !( 0. <= e < 1. ) && throw(ArgumentError("The eccentricity must be within the interval 0 <= e < 1."))
 
     # Tolerance for the Newton-Raphson method.
     tol = 1e-18
@@ -41,49 +36,66 @@ function compute_ss_orbit_by_ang_vel(n::Number, e::Number)
     maxIt = 30
 
     # Auxiliary variables.
-    sqrt_m0::Float64 = sqrt(m0)
-    sqrt_e2::Float64 = sqrt(1-e^2)
+    sqrt_m0 = sqrt(m0)
+    aux_e   = 1-e^2
+    sqrt_e2 = sqrt(aux_e)
 
     # Auxiliary constant to compute the functions.
-    K1::Float64 = 3.0*R0^2*J2*sqrt_m0/(4.0*(1-e^2)^2)
+    K1 = 3R0^2*J2*sqrt_m0/(4aux_e^2)
 
-    # Declare the functions that must solved for 0.
-    f1(a, i) = ne + 2.0*K1*a^(-3.5)*cos(i)
-    f2(a, i) = sqrt_m0*a^(-1.5) + K1*sqrt_e2*(3.0*cos(i)^2-1.0)*a^(-3.5) + K1*(5.0*cos(i)^2-1.0)*a^(-3.5) - n
-
-    # Declare the derivative of the functions.
-    df1da(a, i) = -7.0*K1*a^(-4.5)*cos(i)
-    df1di(a, i) = -2.0*K1*a^(-3.5)*sin(i)
-    df2da(a, i) = -1.5*sqrt_m0*a^(-2.5)-3.5*K1*sqrt_e2*(3.0*cos(i)^2-1.0)*a^(-4.5)-3.5*K1*(5.0*cos(i)^2-1.0)*a^(-4.5)
-    df2di(a, i) = -6.0*K1*sqrt_e2*a^(-3.5)*cos(i)*sin(i)-10.0*K1*a^(-3.5)*cos(i)*sin(i)
-
-    # Jacobian.
-    Jf1f2(a, i) = [df1da(a,i) df1di(a,i);
-                   df2da(a,i) df2di(a,i);]
-
-    ############################################################################
-    # Solve for zeros of f1 and f2 using Newton-Raphson method.
-    ############################################################################
-
-    # Maximum number of
+    # Solve for zeros of f1 and f2 using Newton-Raphson method
+    # ==========================================================================
+    #
+    # The function `f1` is the residue related to the time derivative of RAAN,
+    # and the function `f2` is the residue related to the angular velocity.
 
     # Initial guess based on the unperturbed model.
-    a_k::Float64 = (m0/n^2)^(1/3)
-    i_k::Float64 = acos( -ne*a_k^(3.5)/(2*K1) )
+    a_k = (m0/n^2)^(1/3)
+    i_k = acos( -ne*a_k^(3.5)/(2K1) )
+
+    # By setting the initial values of `f1` and `f2` to `10tol`, we assure that
+    # the loop will be executed at least one time.
+    f1 = 10tol
+    f2 = 10tol
 
     # Loop
-    it = 0;
+    it = 0
     converged = true
 
-    while (abs(f1(a_k, i_k)) > tol) || (abs(f2(a_k, i_k)) > tol)
+    while (abs(f1) > tol) || (abs(f2) > tol)
         a_k_1 = a_k
         i_k_1 = i_k
 
+        # Auxiliary variables.
+        sin_i, cos_i = sincos(i_k_1)
+        sin_i²       = sin_i^2
+        cos_i²       = cos_i^2
+        a_pm1d5      = a_k_1^(-1.5)
+        a_pm2d5      = a_pm1d5*a_k_1^(-1) # --> a_k_1^(-2.5)
+        a_pm3d5      = a_pm2d5*a_k_1^(-1) # --> a_k_1^(-3.5)
+        a_pm4d5      = a_pm3d5*a_k_1^(-1) # --> a_k_1^(-4.5)
+        aux_1        = K1*sqrt_e2*(3cos_i²-1)
+        aux_2        = K1*(5cos_i²-1)
+
         # Compute the Jacobian.
-        J_k_1::Matrix{Float64} = Jf1f2(a_k_1, i_k_1)
+        df1da = -7K1*a_pm4d5*cos_i
+        df1di = -2K1*a_pm3d5*sin_i
+        df2da = -1.5*sqrt_m0*a_pm2d5 - 3.5aux_1*a_pm4d5 - 3.5aux_2*a_pm4d5
+        df2di = -6K1*sqrt_e2*a_pm3d5*cos_i*sin_i - 10K1*a_pm4d5*cos_i*sin_i
+
+        J_k_1 = SMatrix{2,2}(df1da, df1di,
+                             df2da, df2di)'
+
+        # Compute the functions.
+        f1 = ne + 2K1*a_pm3d5*cos_i
+        f2 = sqrt_m0*a_pm1d5 + aux_1*a_pm3d5 + aux_2*a_pm3d5 - n
 
         # Compute the new estimate.
-        (a_k, i_k) = [a_k_1; i_k_1] - inv(J_k_1)*[f1(a_k_1, i_k_1); f2(a_k_1, i_k_1)];
+        vf    = SVector{2}(f1,f2)
+        v_k_1 = SVector{2}(a_k_1, i_k_1)
+        v_k   = v_k_1 - inv(J_k_1)*vf
+        a_k   = v_k[1]
+        i_k   = v_k[2]
 
         # Check if the maximum number of iterations has been reached.
         it += 1
@@ -97,7 +109,7 @@ function compute_ss_orbit_by_ang_vel(n::Number, e::Number)
     end
 
     # Return.
-    a_k, i_k, f1(a_k, i_k), f2(a_k, i_k), converged
+    a_k, i_k, f1, f2, converged
 end
 
 """
