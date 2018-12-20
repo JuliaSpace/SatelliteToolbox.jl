76f27bcf2a4f0d2c72f8365609f699c0c8f4ea33
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2018-12-20 21:38:18 -0200

    :race_horse: Improve the function equation_of_time
    The computational time was reduced by 4%.

--- src/sun/equation_of_time.jl
+++ src/sun/equation_of_time.jl
@@ -25,31 +25,30 @@ algorithm was adapted from [1, p. 178, 277-279].
 """
 function equation_of_time(JD::Number)
     # Constants
-    deg2rad = pi/180
+    deg2rad = π/180
 
     # Number of Julian centuries from J2000 epoch.
     T_UT1 = (JD-JD_J2000)/36525.0
 
     # Mean longitude of the Sun.
-    λ_m = mod(280.460 + 36000.771*T_UT1, 360.0)
+    λ_m = mod(280.460 + 36000.771T_UT1, 360)
 
     # Mean anomaly of the Sun.
     #
     # Here, we should use T_TBD (Barycentric Dynamical Time). However, it is
     # sufficient to use T_UT1 because this is a low precision computation [1].
-    Ms = mod(357.5291092 + 35999.05034*T_UT1, 360.0)
+    Ms = mod(357.5291092 + 35999.05034T_UT1, 360)*deg2rad
+
+    # Auxiliary variables.
+    sin_Ms  = sin(Ms)
+    sin_2Ms = sin(2Ms)
 
     # Ecliptic latitude of the Sun.
-    λ_ecliptic = mod(λ_m + 1.914666471*sin(Ms*deg2rad) +
-                           0.019994643*sin(2*Ms*deg2rad), 360.0)
-
-    # Convert the angles to radians.
-    Ms         *= deg2rad
-    λ_ecliptic *= deg2rad
+    λ_ecliptic = mod(λ_m + 1.914666471sin_Ms + 0.019994643sin_2Ms, 360)*deg2rad
 
     # Compute the equation of time [rad].
-    eot = -1.914666471*sin(Ms) - 0.019994643*sin(2*Ms) +
-           2.466*sin(2*λ_ecliptic) - 0.0053*sin(4*λ_ecliptic)
+    eot = -1.914666471sin_Ms     - 0.019994643sin_2Ms +
+           2.466sin(2λ_ecliptic) - 0.0053sin(4λ_ecliptic)
 
     eot*deg2rad
 end
