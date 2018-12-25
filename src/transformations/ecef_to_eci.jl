6fb68ea2063a4f670662ff5ec07139d95499d63b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2018-12-25 17:09:00 -0200

    :books: Update examples of rECEFtoECI function

--- src/transformations/ecef_to_eci.jl
+++ src/transformations/ecef_to_eci.jl
@@ -111,14 +111,14 @@ julia> eop_IAU1980 = get_iers_eop(:IAU1980);
 julia> rECEFtoECI(DCM, ITRF(), GCRF(), DatetoJD(1986, 06, 19, 21, 35, 0), eop_IAU1980)
 3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
  -0.619267      0.78518     -0.00132979
- -0.78518      -0.619267     3.33483e-5
- -0.000797314   0.00106478   0.999999
+ -0.78518      -0.619267     3.33492e-5
+ -0.000797313   0.00106478   0.999999
 
 julia> rECEFtoECI(ITRF(), GCRF(), DatetoJD(1986, 06, 19, 21, 35, 0), eop_IAU1980)
 3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
  -0.619267      0.78518     -0.00132979
- -0.78518      -0.619267     3.33483e-5
- -0.000797314   0.00106478   0.999999
+ -0.78518      -0.619267     3.33492e-5
+ -0.000797313   0.00106478   0.999999
 
 julia> rECEFtoECI(PEF(), J2000(), DatetoJD(1986, 06, 19, 21, 35, 0))
 3×3 StaticArrays.SArray{Tuple{3,3},Float64,2,9}:
@@ -134,7 +134,7 @@ julia> rECEFtoECI(PEF(), J2000(), DatetoJD(1986, 06, 19, 21, 35, 0), eop_IAU1980
 
 julia> rECEFtoECI(Quaternion, ITRF(), GCRF(), DatetoJD(1986, 06, 19, 21, 35, 0), eop_IAU1980)
 Quaternion{Float64}:
-  + 0.43630989232629747 - 0.0005909971869613186.i + 0.00030510471843995434.j + 0.8997962188693898.k
+  + 0.4363098936462618 - 0.0005909969666939257.i + 0.00030510511316206974.j + 0.8997962182293519.k
 ```
 """
 @inline rECEFtoECI(T_ECEF::T_ECEFs,
