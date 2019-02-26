3467dae4884da8f6d593293d0c6599a087837f3c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2019-02-25 21:06:29 -0300

    :wrench: :books: Improve code and doc of init_orbit_propagator
    The documentation and code style of `init_orbit_propagator`
    function were greatly improved.

--- src/orbit/propagators/api/init_orbit_propagator.jl
+++ src/orbit/propagators/api/init_orbit_propagator.jl
@@ -23,15 +23,18 @@
 export init_orbit_propagator
 
 """
-    function init_orbit_propagator(orbp_type::Type{Val{:J2}}, epoch::Number, a_0::Number, e_0::Number, i_0::Number, Ω_0::Number, ω_0::Number, f_0::Number, dn_o2::Number = 0, ddn_o6::Number = 0, j2_gc::J2_GravCte{T} = j2_gc_wgs84) where T
-    function init_orbit_propagator(::Type{Val{:J4}}, epoch::Number, a_0::Number, e_0::Number, i_0::Number, Ω_0::Number, ω_0::Number, f_0::Number, dn_o2::Number = 0, ddn_o6::Number = 0, j4_gc::J2_GravCte{T} = j4_gc_wgs84) where T
-    function init_orbit_propagator(::Type{Val{:twobody}}, epoch::Number, a_0::Number, e_0::Number, i_0::Number, Ω_0::Number, ω_0::Number, f_0::Number, μ::T = m0) where T
+    function init_orbit_propagator(T, epoch::Number, a_0::Number, e_0::Number, i_0::Number, Ω_0::Number, ω_0::Number, f_0::Number, ...)
+    function init_orbit_propagator(T, orb_0::Orbit, ...)
 
-Initialize the orbit propagator `orbp_type`, which can be:
+Initialize the orbit propagator `T` using the initial mean orbital elements. The
+propagator type `T` can be:
 
 * `Val{:J2}`: J2 orbit propagator;
-* `Val{:J4}`: J4 orbit propagator;
+* `Val{:J4}`: J4 orbit propagator; or
 * `Val{:twobody}`: Two-body orbit propagator.
+
+The mean orbital elements can be passed individually of using an instance of the
+structure `Orbit`.
 
 # Args
 
@@ -44,8 +47,13 @@ Initialize the orbit propagator `orbp_type`, which can be:
 * `f_0`: Initial true anomaly [rad].
 * `n_0`: Initial angular velocity [rad/s].
 * `M_0`: Initial mean anomaly [rad].
+* `orb_0`: Instance of the structure `Orbit` with the initial mean orbital
+           elements [SI].
 
-## J2 and J4 orbit propagator
+## Additional optional arguments for the J2 orbit propagator
+
+The initialization function of the J2 orbit propagator can receive the
+following optional parameters:
 
 * `dn_o2`: (OPTIONAL) First time derivative of mean motion divided by 2
            \\[rad/s²] (**Default** = 0).
@@ -53,10 +61,23 @@ Initialize the orbit propagator `orbp_type`, which can be:
             \\[rad/s³] (**Default** = 0).
 * `j2_gc`: (OPTIONAL) J2 orbit propagator gravitational constants
            (**Default** = `j2_gc_wgs84`).
+
+## Additional optional arguments for the J4 orbit propagator
+
+The initialization function of the J4 orbit propagator can receive the
+following optional parameters:
+
+* `dn_o2`: (OPTIONAL) First time derivative of mean motion divided by 2
+           \\[rad/s²] (**Default** = 0).
+* `ddn_o6`: (OPTIONAL) Second time derivative of mean motion divided by 6
+            \\[rad/s³] (**Default** = 0).
 * `j4_gc`: (OPTIONAL) J4 orbit propagator gravitational constants
            (**Default** = `j4_gc_wgs84`).
 
-## Two-body orbit propagator
+## Additional optional arguments for the two body orbit propagator
+
+The initialization function of the two body orbit propagator can receive the
+following optional parameter:
 
 * `μ`: (OPTIONAL) Standard gravitational parameter of the central body
        \\[m^3/s^2] (**Default** = `m0`).
@@ -68,22 +89,14 @@ the orbit propagator.
 
 # Remarks
 
-## J2, J4, and Two Body propagators
-
 If the orbit is defined in terms of the angular velocity (mean motion) instead
 of the semi-major axis, then it is possible to use the function `angvel_to_a` to
 convert.
 
 """
-function init_orbit_propagator(::Type{Val{:J2}},
-                               epoch::Number,
-                               a_0::Number,
-                               e_0::Number,
-                               i_0::Number,
-                               Ω_0::Number,
-                               ω_0::Number,
-                               f_0::Number,
-                               dn_o2::Number = 0,
+function init_orbit_propagator(::Type{Val{:J2}}, epoch::Number, a_0::Number,
+                               e_0::Number, i_0::Number, Ω_0::Number,
+                               ω_0::Number, f_0::Number, dn_o2::Number = 0,
                                ddn_o6::Number = 0,
                                j2_gc::J2_GravCte{T} = j2_gc_wgs84) where T
 
@@ -98,15 +111,9 @@ function init_orbit_propagator(::Type{Val{:J2}},
     OrbitPropagatorJ2(orb_0, j2d)
 end
 
-function init_orbit_propagator(::Type{Val{:J4}},
-                               epoch::Number,
-                               a_0::Number,
-                               e_0::Number,
-                               i_0::Number,
-                               Ω_0::Number,
-                               ω_0::Number,
-                               f_0::Number,
-                               dn_o2::Number = 0,
+function init_orbit_propagator(::Type{Val{:J4}}, epoch::Number, a_0::Number,
+                               e_0::Number, i_0::Number, Ω_0::Number,
+                               ω_0::Number, f_0::Number, dn_o2::Number = 0,
                                ddn_o6::Number = 0,
                                j4_gc::J4_GravCte{T} = j4_gc_wgs84) where T
 
@@ -121,15 +128,11 @@ function init_orbit_propagator(::Type{Val{:J4}},
     OrbitPropagatorJ4(orb_0, j4d)
 end
 
-function init_orbit_propagator(::Type{Val{:twobody}},
-                               epoch::Number,
-                               a_0::Number,
-                               e_0::Number,
-                               i_0::Number,
-                               Ω_0::Number,
-                               ω_0::Number,
-                               f_0::Number,
+function init_orbit_propagator(::Type{Val{:twobody}}, epoch::Number,
+                               a_0::Number, e_0::Number, i_0::Number,
+                               Ω_0::Number, ω_0::Number, f_0::Number,
                                μ::T = m0) where T
+
     # Create the new Two Body propagator structure.
     tbd = twobody_init(epoch, a_0, e_0, i_0, Ω_0, ω_0, f_0, μ)
 
@@ -140,136 +143,62 @@ function init_orbit_propagator(::Type{Val{:twobody}},
     OrbitPropagatorTwoBody(orb_0, tbd)
 end
 
-"""
-    function init_orbit_propagator(orbp_type::Type{Val{:J2}}, orb_0::Orbit, dn_o2::Number = 0, ddn_o6::Number = 0, j2_gc::J2_GravCte = j2_gc_wgs84)
-    function init_orbit_propagator(orbp_type::Type{Val{:J4}}, orb_0::Orbit, dn_o2::Number = 0, ddn_o6::Number = 0, j4_gc::J4_GravCte = j4_gc_wgs84)
-    function init_orbit_propagator(orbp_type::Type{Val{:twobody}}, orb_0::Orbit, μ::Number = m0)
+init_orbit_propagator(::Type{Val{:J2}}, orb_0::Orbit, dn_o2::Number = 0,
+                      ddn_o6::Number = 0, j2_gc::J2_GravCte = j2_gc_wgs84) =
+    init_orbit_propagator(Val{:J2}, orb_0.t, orb_0.a, orb_0.e, orb_0.i, orb_0.Ω,
+                          orb_0.ω, orb_0.f, dn_o2, ddn_o6, j2_gc)
 
-Initialize the orbit propagator `orbp_type`, which can be:
+init_orbit_propagator(::Type{Val{:J4}}, orb_0::Orbit, dn_o2::Number = 0,
+                      ddn_o6::Number = 0, j4_gc::J4_GravCte = j4_gc_wgs84) =
+    init_orbit_propagator(Val{:J4}, orb_0.t, orb_0.a, orb_0.e, orb_0.i, orb_0.Ω,
+                          orb_0.ω, orb_0.f, dn_o2, ddn_o6, j4_gc)
+
+init_orbit_propagator(::Type{Val{:twobody}}, orb_0::Orbit, μ::Number = m0) =
+    init_orbit_propagator(Val{:twobody}, orb_0.t, orb_0.a, orb_0.e, orb_0.i,
+                          orb_0.Ω, orb_0.ω, orb_0.f, μ)
+
+"""
+    function init_orbit_propagator(T, tle::TLE, ...)
+
+Initialize the orbit propagator `T` using the TLE `tle`. The propagator type `T`
+can be:
 
 * `Val{:J2}`: J2 orbit propagator;
 * `Val{:J4}`: J4 orbit propagator;
-* `Val{:sgp4}`: SGP4 orbit propagator; or
-* `Val{:twobody}`: Two-body orbit propagator.
+* `Val{:twobody}`: Two-body orbit propagator; or
+* `Val{:sgp4}`: SGP4 orbit propagator.
 
-# Args
+## Additional optional arguments for the J2 orbit propagator
 
-* `orb_0`: Initial orbital elements (see `Orbit`).
+The initialization function of the J2 orbit propagator can receive the
+following optional parameter:
 
-## J2 and J4 orbit propagator
-
-* `dn_o2`: (OPTIONAL) First time derivative of mean motion divided by 2
-           \\[rad/s²] (**Default** = 0).
-* `ddn_o6`: (OPTIONAL) Second time derivative of mean motion divided by 6
-            \\[rad/s³] (**Default** = 0).
 * `j2_gc`: (OPTIONAL) J2 orbit propagator gravitational constants
            (**Default** = `j2_gc_wgs84`).
+
+## Additional optional arguments for the J4 orbit propagator
+
+The initialization function of the J4 orbit propagator can receive the
+following optional parameter:
+
 * `j4_gc`: (OPTIONAL) J4 orbit propagator gravitational constants
            (**Default** = `j4_gc_wgs84`).
 
-## SGP4 orbit propagator
+## Additional optional arguments for the two body orbit propagator
 
-* `bstar`: (OPTIONAL) B* parameter of the SGP4 (**Default** = 0).
-* `sgp4_gc`: (OPTIONAL) Gravitational constants (**Default** = `sgp4_gc_wgs84`).
-
-## Two-body orbit propagator
-
-* `μ`: (OPTIONAL) Standard gravitational parameter of the central body [m^3/s^2]
-       \\(**Default** = `m0`).
-
-# Returns
-
-A new instance of the orbit propagator structure that stores the information of
-the orbit propagator.
-
-# Remarks
-
-## SGP4 Orbit Propagator
-
-Notice that the orbit elements specified in `orb_0` **must be** represented in
-TEME frame.
-
-This implementation includes also the deep space perturbations, which was
-originally called SDP4 algorithm. Modern approaches, such as [2] and [3],
-identifies if the selected orbit must be propagated using the deep space
-perturbations and automatically applied them. This is sometimes called SGDP4
-algorithm.
-
-"""
-function init_orbit_propagator(::Type{Val{:J2}},
-                               orb_0::Orbit,
-                               dn_o2::Number = 0,
-                               ddn_o6::Number = 0,
-                               j2_gc::J2_GravCte = j2_gc_wgs84)
-    init_orbit_propagator(Val{:J2},
-                          orb_0.t,
-                          orb_0.a,
-                          orb_0.e,
-                          orb_0.i,
-                          orb_0.Ω,
-                          orb_0.ω,
-                          orb_0.f,
-                          dn_o2,
-                          ddn_o6,
-                          j2_gc)
-end
-
-function init_orbit_propagator(::Type{Val{:J4}},
-                               orb_0::Orbit,
-                               dn_o2::Number = 0,
-                               ddn_o6::Number = 0,
-                               j4_gc::J4_GravCte = j4_gc_wgs84)
-    init_orbit_propagator(Val{:J4}, orb_0.t, orb_0.a, orb_0.e, orb_0.i, orb_0.Ω,
-                      orb_0.ω, orb_0.f, dn_o2, ddn_o6, j4_gc)
-end
-
-function init_orbit_propagator(::Type{Val{:twobody}},
-                               orb_0::Orbit,
-                               μ::Number = m0)
-    init_orbit_propagator(Val{:twobody},
-                          orb_0.t,
-                          orb_0.a,
-                          orb_0.e,
-                          orb_0.i,
-                          orb_0.Ω,
-                          orb_0.ω,
-                          orb_0.f,
-                          μ)
-end
-
-"""
-    function init_orbit_propagator(orbp_type::Type{Val{:J2}}, tle::TLE, j2_gc::J2_GravCte = j2_gc_wgs84)
-    function init_orbit_propagator(orbp_type::Type{Val{:J4}}, tle::TLE, j4_gc::J4_GravCte = j4_gc_wgs84)
-    function init_orbit_propagator(orbp_type::Type{Val{:sgp4}}, tle::TLE, sgp4_gc::SGP4_Structure = sgp4_gc_wgs84)
-    function init_orbit_propagator(orbp_type::Type{Val{:twobody}}, tle::TLE, μ::Number = m0)
-
-Initialize the orbit propagator `orbp_type`, which can be:
-
-* `Val{:J2}`: J2 orbit propagator;
-* `Val{:J4}`: J4 orbit propagator;
-* `Val{:sgp4}`: SGP4 orbit propagator; or
-* `Val{:twobody}`: Two-body orbit propagator.
-
-# Args
-
-* `tle`: TLE that will be used to initialize the propagator.
-
-## J2 orbit propagator
-
-* `j2_gc`: (OPTIONAL) J2 orbit propagator gravitational constants (**Default** =
-           `j2_gc_wgs84`).
-* `j4_gc`: (OPTIONAL) J4 orbit propagator gravitational constants
-           (**Default** = `j4_gc_wgs84`).
-
-## SGP4 orbit propagator
-
-* `sgp4_gc`: (OPTIONAL) Gravitational constants (**Default** = `sgp4_gc_wgs84`).
-
-# Two-body orbit propagator
+The initialization function of the two body orbit propagator can receive the
+following optional parameter:
 
 * `μ`: (OPTIONAL) Standard gravitational parameter of the central body
        \\[m^3/s^2] (**Default** = `m0`).
 
+## Additional optional arguments for the SGP4 orbit propagator
+
+The initialization function of the SGP4 orbit propagator can receive the
+following optional parameter:
+
+* `sgp4_gc`: (OPTIONAL) Gravitational constants (**Default** = `sgp4_gc_wgs84`).
+
 # Returns
 
 A new instance of the orbit propagator structure that stores the information of
@@ -277,9 +206,7 @@ the orbit propagator.
 
 # Remarks
 
-## SGP4 Orbit Propagator
-
-This implementation includes also the deep space perturbations, which was
+The SGP4 implementation includes also the deep space perturbations, which was
 originally called SDP4 algorithm. Modern approaches, such as [2] and [3],
 identifies if the selected orbit must be propagated using the deep space
 perturbations and automatically applied them. This is sometimes called SGDP4
@@ -372,35 +299,6 @@ function init_orbit_propagator(::Type{Val{:J4}},
                           j4_gc_wgs84)
 end
 
-function init_orbit_propagator(::Type{Val{:sgp4}}, tle::TLE,
-                               sgp4_gc::SGP4_GravCte{T} = sgp4_gc_wgs84) where T
-
-    # Constants.
-    d2r           =  π/180      # Degrees to radians.
-    revday2radmin = 2π/1440     # Revolutions per day to radians per minute.
-
-    # Obtain the data from the TLE.
-    epoch = tle.epoch
-    n_0   = tle.n*revday2radmin
-    e_0   = tle.e
-    i_0   = tle.i*d2r
-    Ω_0   = tle.Ω*d2r
-    ω_0   = tle.ω*d2r
-    M_0   = tle.M*d2r
-    bstar = tle.bstar
-
-    # Create the new SGP4 structure.
-    sgp4d = sgp4_init(sgp4_gc, tle.epoch, n_0, e_0, i_0, Ω_0, ω_0, M_0,
-                      tle.bstar)
-
-    # Create the `Orbit` structure.
-    orb_0 = Orbit{T,T,T,T,T,T,T}(epoch, 1000sgp4d.a_k*sgp4_gc.R0, e_0, i_0, Ω_0,
-                                 ω_0, M_to_f(e_0, M_0))
-
-    # Create and return the orbit propagator structure.
-    OrbitPropagatorSGP4(orb_0, sgp4_gc, sgp4d)
-end
-
 function init_orbit_propagator(::Type{Val{:twobody}},
                                tle::TLE,
                                μ::Number = m0)
@@ -433,3 +331,33 @@ function init_orbit_propagator(::Type{Val{:twobody}},
                           f_0,
                           μ)
 end
+
+function init_orbit_propagator(::Type{Val{:sgp4}}, tle::TLE,
+                               sgp4_gc::SGP4_GravCte{T} = sgp4_gc_wgs84) where T
+
+    # Constants.
+    d2r           =  π/180      # Degrees to radians.
+    revday2radmin = 2π/1440     # Revolutions per day to radians per minute.
+
+    # Obtain the data from the TLE.
+    epoch = tle.epoch
+    n_0   = tle.n*revday2radmin
+    e_0   = tle.e
+    i_0   = tle.i*d2r
+    Ω_0   = tle.Ω*d2r
+    ω_0   = tle.ω*d2r
+    M_0   = tle.M*d2r
+    bstar = tle.bstar
+
+    # Create the new SGP4 structure.
+    sgp4d = sgp4_init(sgp4_gc, tle.epoch, n_0, e_0, i_0, Ω_0, ω_0, M_0,
+                      tle.bstar)
+
+    # Create the `Orbit` structure.
+    orb_0 = Orbit{T,T,T,T,T,T,T}(epoch, 1000sgp4d.a_k*sgp4_gc.R0, e_0, i_0, Ω_0,
+                                 ω_0, M_to_f(e_0, M_0))
+
+    # Create and return the orbit propagator structure.
+    OrbitPropagatorSGP4(orb_0, sgp4_gc, sgp4d)
+end
+
