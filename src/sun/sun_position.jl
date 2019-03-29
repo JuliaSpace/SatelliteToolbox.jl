52f2c040ce92c09c5f5222eb682a8881dd7fd041
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2019-03-29 15:48:41 -0300

    :racehorse: Improve Sun position algorithm
    The performance of the Sun position algorithm was increased by 15%.

--- src/sun/sun_position.jl
+++ src/sun/sun_position.jl
@@ -29,42 +29,48 @@ Julian Day `JD`. The algorithm was adapted from [3, p. 277-279].
 
 """
 function sun_position_i(JD::Number)
+    # Constants.
+    deg2rad = π/180
+
     # Number of Julian centuries from J2000 epoch.
     T_UT1 = (JD-JD_J2000)/36525.0
 
-    # Mean longitude of the Sun [deg].
-    λ_m = mod(280.460 + 36000.771T_UT1, 360)
+    # Here, we will assume that T_TBD ≈ T_UT1. This can be done because we are
+    # using a low-precision algorithm [3].
+    T_TBD = T_UT1
 
     # Mean anomaly of the Sun [deg].
-    #
-    # Here, we should use T_TBD (Barycentric Dynamical Time). However, it is
-    # sufficient to use T_UT1 because this is a low precision computation [1].
-    Ms = mod(357.5291092 + 35999.05034T_UT1, 360)
+    Ms = 357.529_109_2 + 35_999.050_34T_TBD
 
-    # Convert Ms to [rad].
-    Ms = deg2rad(Ms)
+    # Convert Ms to [rad] and limit to the interval [0,2π].
+    Ms = mod2pi(Ms*deg2rad)
+
+    # Compute auxiliary variables.
+    sinMs,  cosMs  = sincos(Ms)
+    sin2Ms, cos2Ms = sincos(2Ms)
+
+    # Mean longitude of the Sun [deg].
+    λ_m = 280.460 + 36_000.771T_UT1
 
     # Ecliptic latitude of the Sun [deg].
-    λ_e = mod(λ_m + 1.914666471sin(Ms) + 0.019994643sin(2Ms), 360)
+    λ_e = λ_m + 1.914_666_471sinMs + 0.019_994_643sin2Ms
 
     # Obliquity of the ecliptic [deg].
-    #
-    # Here, we are also approximating T_TBD ≈ T_UT1.
-    ϵ = mod(23.439291 - 0.0130042T_UT1, 360)
+    ϵ = 23.439_291 - 0.013_004_2T_TBD
+
+    # Convert λ_e and ϵ to [rad] and limit to the interval [0,2π].
+    λ_e = mod2pi(λ_e*deg2rad)
+    ϵ   = mod2pi(  ϵ*deg2rad)
 
     # Auxiliary variables.
-    sin_Ms  , cos_Ms  = sincos(Ms)
-    sin_2Ms , cos_2Ms = sincos(2Ms)
-    sin_ϵ   , cos_ϵ   = sincos(deg2rad(ϵ))
-    sin_λ_e , cos_λ_e = sincos(deg2rad(λ_e))
+    sinϵ   , cosϵ   = sincos(ϵ)
+    sinλ_e , cosλ_e = sincos(λ_e)
 
     # Distance of the Sun from Earth [m].
-    r = ( 1.000140612 - 0.016708617cos_Ms - 0.000139589cos_2Ms )*au2m
+    r = ( 1.000_140_612 - 0.016_708_617cosMs - 0.000_139_589cos2Ms )*au2m
 
     # Compute the Sun vector represented in the Mean Equinox of Date (MOD).
-    S_MOD = SVector{3,Float64}(      r*cos_λ_e,
-                               r*cos_ϵ*sin_λ_e,
-                               r*sin_ϵ*sin_λ_e)
+    S_MOD = SVector{3,Float64}(r*cosλ_e, r*cosϵ*sinλ_e, r*sinϵ*sinλ_e)
 
     # TODO: This vector must be transformed to the inertial reference frame that
     # will be used in this toolbox (J2000, True of Date, etc.).
