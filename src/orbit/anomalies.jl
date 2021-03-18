32deb9c6bc2ba3a60ccbfd7f717b0cc3a492edbc
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-03-17 21:40:51 -0300

    :wrench: Improve code related to the anomalies

--- src/orbit/anomalies.jl
+++ src/orbit/anomalies.jl
@@ -51,7 +51,7 @@ tolerance to accept the solution is `tol`.
     end
 
     # Return the eccentric anomaly in the interval [0, 2π].
-    mod(E, 2π)
+    return mod(E, 2π)
 end
 
 """
@@ -67,7 +67,7 @@ tolerance to accept the solution is `tol`.
     E = M_to_E(e, M, tol)
 
     # Compute the true anomaly in the interval [0,2π].
-    E_to_f(e,E)
+    return E_to_f(e,E)
 end
 
 ################################################################################
@@ -85,7 +85,7 @@ eccentric anomaly `E` [rad].
     sin_Eo2, cos_Eo2 = sincos(E/2)
 
     # Compute the true anomaly in the interval [0, 2*π].
-    mod( 2atan(sqrt(1+e)*sin_Eo2, sqrt(1-e)*cos_Eo2) , 2π )
+    return mod( 2atan(sqrt(1+e)*sin_Eo2, sqrt(1-e)*cos_Eo2) , 2π )
 end
 
 """
@@ -95,7 +95,7 @@ Compute the mean anomaly (0,2π) \\[rad] given the eccentricity `e` and the
 eccentric anomaly `E` [rad].
 
 """
-E_to_M(e::Number, E::Number) = mod(E - e*sin(E), 2π)
+@inline E_to_M(e::Number, E::Number) = mod(E - e*sin(E), 2π)
 
 ################################################################################
 #                              From True Anomaly
@@ -111,17 +111,8 @@ the true anomaly `f` [rad].
 @inline function f_to_E(e::Number, f::Number)
     sin_fo2, cos_fo2 = sincos(f/2)
 
-    mod( 2atan(sqrt(1-e)*sin_fo2, sqrt(1+e)*cos_fo2), 2π )
+    return mod( 2atan(sqrt(1-e)*sin_fo2, sqrt(1+e)*cos_fo2), 2π )
 end
-
-"""
-    f_to_E(orb::Orbit)
-
-Compute the eccentric anomaly (0,2π) \\[rad] given the orbit `orb` (see
-`Orbit`).
-
-"""
-f_to_E(orb::Orbit) = f_to_E(orb.e, orb.f)
 
 """
     f_to_M(e::Number, f::Number)
@@ -135,13 +126,5 @@ true anomaly `f` [rad].
     E = f_to_E(e, f)
 
     # Compute the true anomaly in the interval [0, 2π].
-    E_to_M(e,E)
+    return E_to_M(e,E)
 end
-
-"""
-    f_to_M(orb::Orbit)
-
-Compute the mean anomaly (0,2π) \\[rad] given the orbit `orb` (see `Orbit`).
-
-"""
-f_to_M(orb::Orbit) = f_to_M(orb.e, orb.f)
