7eac2f8bff7220dfec403e902a980e4606a17337
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-03-18 13:33:47 -0300

    :wrench: Conversion between orbit representations

--- src/orbit/representation/conversions.jl
+++ src/orbit/representation/conversions.jl
@@ -13,10 +13,16 @@ Base.convert(::Type{KeplerianElements{T}}, k::KeplerianElements) where T =
 Base.convert(::Type{OrbitStateVector{T}}, sv::OrbitStateVector) where T =
     OrbitStateVector{T}(sv.t, sv.r, sv.v, sv.a)
 
+Base.convert(::Type{KeplerianElements}, sv::OrbitStateVector{T}) where T =
+    Base.convert(KeplerianElements{T}, sv)
+
 function Base.convert(KT::Type{KeplerianElements{T}}, sv::OrbitStateVector) where T
     k = sv_to_kepler(sv)
     return convert(KT, k)
 end
+
+Base.convert(ST::Type{OrbitStateVector}, k::KeplerianElements{T}) where T =
+    Base.convert(KeplerianElements{T}, sv)
 
 function Base.convert(ST::Type{OrbitStateVector{T}}, k::KeplerianElements) where T
     sv = kepler_to_sv(k)
