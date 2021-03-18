477436c468524889adb067af3aad96d11fff61a8
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-03-18 15:01:49 -0300

    :wrench: Add orb. repr. support to change_oe_frame

--- src/transformations/orbit_elements.jl
+++ src/transformations/orbit_elements.jl
@@ -30,7 +30,7 @@ export change_oe_frame
 
 Change the reference frame of orbit elements. The orbit elements can be
 specified by `a`, `e`, `i`, `Ω`, `ω`, and `f`, or the structure `oe` (see
-`Orbit`).
+[`Orbit`](@ref)). In the latter, the return value type will match the type of `oe`.
 
 The conversion arguments `conv_args` are **the same** arguments that one should
 pass to the function `rECItoECI` to convert between the desired frames. For more
@@ -48,13 +48,14 @@ information, see the documentation of the function `rECItoECI`.
                   would pass to the function `rECItoECI` to convert between the
                   desired frames.
 
-* `oe`: An instance of the structure `Orbit` with the orbit elements that will
-        be converted [SI units].
+* `oe`: An instance of the structure [`Orbit`](@ref) with the orbit elements
+        that will be converted [SI units].
 
 # Returns
 
-An instance of the structure `Orbit` with the Keplerian elements [SI units]
-converted to the new frame.
+Using the first signature, this function returns an instance of
+[`KeplerianElements`](@ref). If the second signature is used, then the function
+return an element with the same type of the input.
 
 # Examples
 
@@ -65,47 +66,41 @@ julia> teme_epoch = DatetoJD(2016,6,1,11,0,0);
 
 julia> tod_epoch  = DatetoJD(2016,1,1,0,0,0);
 
-julia> oe_teme    = Orbit(0,
-                          7130.982e3,
-                          0.001111,
-                          98.405*pi/180,
-                          227.336*pi/180,
-                          90*pi/180,
-                          320*pi/180)
+julia> k_teme     = KeplerianElements(teme_epoch,
+                                      7130.982e3,
+                                      0.001111,
+                                      98.405*pi/180,
+                                      227.336*pi/180,
+                                      90*pi/180,
+                                      320*pi/180)
+KeplerianElements{Float64}:
+           Epoch :    2.45754e6 (2016-06-01T11:00:00)
+ Semi-major axis : 7130.98     km
+    Eccentricity :    0.001111
+     Inclination :   98.405    °
+            RAAN :  227.336    °
+ Arg. of Perigee :   90.0      °
+    True Anomaly :  320.0      °
 
-                 Orbit
-  =====================================
-                  t =        0.0
-    Semi-major axis =     7130.9820 km
-       Eccentricity =        0.001111
-        Inclination =       98.4050 ˚
-               RAAN =      227.3360 ˚
-    Arg. of Perigee =       90.0000 ˚
-       True Anomaly =      320.0000 ˚
+julia> k_j2000 = change_oe_frame(k_teme, TEME(), J2000(), teme_epoch, eop)
+KeplerianElements{Float64}:
+           Epoch :    2.45754e6 (2016-06-01T11:00:00)
+ Semi-major axis : 7130.98     km
+    Eccentricity :    0.001111
+     Inclination :   98.3365   °
+            RAAN :  227.134    °
+ Arg. of Perigee :   90.0604   °
+    True Anomaly :  320.0      °
 
-julia> oe_j2000 = change_oe_frame(oe_teme, TEME(), J2000(), teme_epoch, eop)
-
-                 Orbit
-  ======================================
-                  t =        0.0
-    Semi-major axis =     7130.9820 km
-       Eccentricity =        0.001111
-        Inclination =       98.3365 ˚
-               RAAN =      227.1345 ˚
-    Arg. of Perigee =       90.0604 ˚
-       True Anomaly =      320.0000 ˚
-
-julia> oe_tod   = change_oe_frame(oe_teme, TEME(), teme_epoch, TOD(), tod_epoch, eop)
-
-                 Orbit
-  ======================================
-                  t =        0.0
-    Semi-major axis =     7130.9820 km
-       Eccentricity =        0.001111
-        Inclination =       98.4037 ˚
-               RAAN =      227.3306 ˚
-    Arg. of Perigee =       90.0014 ˚
-       True Anomaly =      320.0000 ˚
+julia> k_tod = change_oe_frame(k_teme, TEME(), teme_epoch, TOD(), tod_epoch, eop)
+KeplerianElements{Float64}:
+           Epoch :    2.45754e6 (2016-06-01T11:00:00)
+ Semi-major axis : 7130.98     km
+    Eccentricity :    0.001111
+     Inclination :   98.4037   °
+            RAAN :  227.331    °
+ Arg. of Perigee :   90.0014   °
+    True Anomaly :  320.0      °
 ```
 """
 function change_oe_frame(a::Number,
@@ -119,19 +114,29 @@ function change_oe_frame(a::Number,
     # The approach is to transform the orbit elements to Cartesian
     # representation, convert the frame, and then convert back to orbit
     # elements.
-    #
+
+    k = KeplerianElements(0, a, e, i, Ω, ω, f)
+
+    return change_oe_frame(k, conv_args...)
+end
+
+function change_oe_frame(oe::T, conv_args...) where T<:Orbit
+    # First, we need to convert to state vector.
+    sv_o = convert(OrbitStateVector, oe)
+    r_o  = sv_o.r
+    v_o  = sv_o.v
+    a_o  = sv_o.a
+
     # NOTE: In my benchmarks, the operation with DCMs are faster than with
     # quaternions after the DCM representation was changed to SMatrix.
-
-    r_o, v_o    = kepler_to_rv(a, e, i, Ω, ω, f)
     D_ECIf_ECIo = rECItoECI(DCM, conv_args...)
     r_f         = D_ECIf_ECIo*r_o
     v_f         = D_ECIf_ECIo*v_o
+    a_f         = D_ECIf_ECIo*a_o
 
-    return rv_to_kepler(r_f, v_f)
-end
+    # Create the new state vector that will be converted to an entity with the
+    # type `T`.
+    sv_f = orbsv(sv_o.t, r_f, v_f, a_f)
 
-function change_oe_frame(oe::Orbit, conv_args...)
-    k = convert(KeplerianElements, oe)
-    return change_oe_frame(k.a, k.e, k.i, k.Ω, k.ω, k.f, conv_args...)
+    return convert(T, sv_f)
 end
