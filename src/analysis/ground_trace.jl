e51cfc51c9863cbb908f598c76a7dd85f8ba0584
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-02 20:59:53 -0300

    :warning: New API for the ground_trace function

--- src/analysis/ground_trace.jl
+++ src/analysis/ground_trace.jl
@@ -9,9 +9,10 @@
 export ground_trace
 
 """
-    ground_trace(orbp::OrbitPropagator{N}, eop_data::Union{Nothing, EOPData_IAU1980, EOPData_IAU2000A} = nothing; ECI = TEME(), ECEF = PEF(), span = 1.0) where N
+    ground_trace(orbp::OrbitPropagator, Δt::Number; kwargs...)
 
-Compute the ground trace of the object with orbit defined by `orbp`.
+Compute the ground trace of the object with orbit defined by `orbp` by
+propagating the orbit by `Δt` [s] from the orbit epoch.
 
 By default, it considers that the orbit elements on the propagator are
 represented in the True Equator, Mean Equinox (TEME) reference frame and the
@@ -28,8 +29,6 @@ presented as follows.
          (**Default** = `TEME()`)
 * `ECEF`: ECEF frame that will be used to compute the ground trace.
           (**Default** = `PEF()`)
-* `span`: Defines for how much time the ground trace will be computed. The unit
-          is the orbit period. (**Default** = 1.0)
 * `dt`: Time interval between two samples [s]. (**Default** = 10.0)
 
 # Returns
@@ -37,21 +36,23 @@ presented as follows.
 A vector of tuples with the pairs `(latitude,longitude)` of the ground trace.
 
 """
-function ground_trace(orbp::OrbitPropagator{N};
+function ground_trace(orbp::OrbitPropagator, Δt::Number;
                       eop_data::Union{Nothing, EOPData_IAU1980, EOPData_IAU2000A} = nothing,
-                      ECI = TEME(), ECEF = PEF(), span = 1.0, dt = 10.0) where N
-
-    # Get the orbit period.
-    T = period(orbp.orb, :J2)
+                      ECI = TEME(),
+                      ECEF = PEF(),
+                      dt::Number = 10)
 
     # Copy orbit structure so that it is not modified by `propagate`.
     orbp_c = deepcopy(orbp)
 
+    # Timespan of the analysis.
+    t = 0:dt:Δt
+
     # Compute the points represented in the inertial reference frame.
-    (o, r_i, ~) = propagate!(orbp_c, 0.0:dt:T*span)
+    r_i, ~ = propagate!(orbp_c, t)
 
     # Get the epochs in Julian Day of each instant.
-    JD = map(x->x.t, o)
+    JD = get_epoch(orbp) .+ t./86400
 
     # Convert from the ECI to the ECEF frame.
     if eop_data == nothing
