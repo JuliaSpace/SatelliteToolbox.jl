053d3ccf7b4150ffc62a4ea35bb1b69b103b4c79
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-07 23:57:13 -0300

    :racehorse: Improve fast moon position algorithm
    The execution time was reduced by 33%.

--- src/moon/moon_position.jl
+++ src/moon/moon_position.jl
@@ -172,27 +172,26 @@ function moon_position_i(JD_TDB::Number, ::Val{:Meeus})
 end
 
 function moon_position_i(JD_TDB::Number, ::Val{:fast})
-    # Constants.
-    deg2rad = π / 180
-
     # Number of Julian centuries from J2000 epoch.
     T_TDB = (JD_TDB - JD_J2000)/36525.0
 
     # Auxiliary computation to improve performance.
-    sin1, cos1 = sincos((134.9 + 477_198.85T_TDB)*deg2rad)
-    sin2, cos2 = sincos((259.2 - 413_335.38T_TDB)*deg2rad)
-    sin3, cos3 = sincos((235.7 + 890_534.23T_TDB)*deg2rad)
-    sin4, cos4 = sincos((269.9 + 954_397.70T_TDB)*deg2rad)
-    sin5       = sind(357.5 +  35_999.05T_TDB)
-    sin6       = sind(186.6 + 966_404.05T_TDB)
+    sin1, cos1 = sincos(deg2rad(134.9 + 477_198.85T_TDB))
+    sin2, cos2 = sincos(deg2rad(259.2 - 413_335.38T_TDB))
+    sin3, cos3 = sincos(deg2rad(235.7 + 890_534.23T_TDB))
+    sin4, cos4 = sincos(deg2rad(269.9 + 954_397.70T_TDB))
+    sin5       = sin(deg2rad(357.5 +  35_999.05T_TDB))
+    sin6       = sin(deg2rad(186.6 + 966_404.05T_TDB))
 
     # Ecliptic latitude of the Moon [deg].
     λₑ = 218.32 + 481_267.8813T_TDB +
          6.29sin1 - 1.27sin2 + 0.66sin3 + 0.21sin4 - 0.19sin5 - 0.11sin6
 
     # Ecliptic longitude of the Moon [deg].
-    ϕₑ = 5.13sind( 93.3 + 483_202.03T_TDB) + 0.28sind(228.2 + 960_400.87T_TDB) -
-         0.28sind(318.3 +   6_003.18T_TDB) - 0.17sind(217.6 - 407_332.20T_TDB)
+    ϕₑ = 5.13sin(deg2rad( 93.3 + 483_202.03T_TDB)) +
+         0.28sin(deg2rad(228.2 + 960_400.87T_TDB)) -
+         0.28sin(deg2rad(318.3 +   6_003.18T_TDB)) -
+         0.17sin(deg2rad(217.6 - 407_332.20T_TDB))
 
     # Parallax [deg].
     P = 0.9508 + 0.0518cos1 + 0.0095cos2 + 0.0078cos3 + 0.0028cos4
@@ -201,10 +200,10 @@ function moon_position_i(JD_TDB::Number, ::Val{:fast})
     ϵ = @evalpoly(T_TDB, 23.439_291, -0.013_004_2, -1.64e-7, +5.04e-7)
 
     # Convert to radians and limit to the interval [0,2π].
-    λₑ = mod2pi(λₑ * deg2rad)
-    ϕₑ = mod2pi(ϕₑ * deg2rad)
-    P  = mod2pi( P * deg2rad)
-    ϵ  = mod2pi( ϵ * deg2rad)
+    λₑ = mod2pi(deg2rad(λₑ))
+    ϕₑ = mod2pi(deg2rad(ϕₑ))
+    P  = mod2pi(deg2rad( P))
+    ϵ  = mod2pi(deg2rad( ϵ))
 
     # Compute the distance from Earth to the Moon [m].
     r = R0 / sin(P)
