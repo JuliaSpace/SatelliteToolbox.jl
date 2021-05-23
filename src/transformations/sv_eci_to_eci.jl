ba1625004849f0134684f4643222f4372c90aae7
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-22 21:29:47 -0300

    :art: Apply BlueStyle to s/t/sv_eci_to_eci.jl

--- src/transformations/sv_eci_to_eci.jl
+++ src/transformations/sv_eci_to_eci.jl
@@ -22,14 +22,13 @@ export sv_eci_to_eci
     sv_eci_to_eci(sv::OrbitStateVector, args...)
 
 Convert the orbit state vector `sv` from an ECI frame to another ECI frame. The
-arguments `args...` must match those of the function `r_eci_to_eci` **wihtout**
-the rotation representation.
-
+arguments `args...` must match those of the function [`r_eci_to_eci`](@ref)
+**wihtout** the rotation representation.
 """
 function sv_eci_to_eci(sv::OrbitStateVector, args...)
     D = r_eci_to_eci(DCM, args...)
 
     # Since both frames does not have a significant angular velocity between
     # them, then we just need to convert the representations.
-    return OrbitStateVector(t = sv.t, r = D*sv.r, v = D*sv.v, a = D*sv.a)
+    return OrbitStateVector(t = sv.t, r = D * sv.r, v = D * sv.v, a = D * sv.a)
 end
