f8cfc208b0ed8435a37d046b507b28cdffe5866a
Alberto Mengali <disberd@gmail.com>
2021-08-18 16:50:28 +0200

    Modify existing geodetic_geocentric functions

--- src/transformations/geodetic_geocentric.jl
+++ src/transformations/geodetic_geocentric.jl
@@ -31,10 +31,10 @@ export ecef_to_geodetic, geodetic_to_ecef
 export geocentric_to_geodetic, geodetic_to_geocentric
 
 """
-    ecef_to_geodetic(r_e::AbstractVector)
+    ecef_to_geodetic(r_e::AbstractVector; ellipsoid=wgs84_ellipsoid)
 
 Convert the vector `r_e` [m] represented in the Earth-Centered, Earth-Fixed
-(ECEF) reference frame into Geodetic coordinates (WGS-84).
+(ECEF) reference frame into Geodetic coordinates for a custom target ellipsoid (defaults to WGS-84).
 
 !!! info
     The algorithm is based in **[3]**.
@@ -50,46 +50,49 @@ Convert the vector `r_e` [m] represented in the Earth-Centered, Earth-Fixed
 - **[3]**: mu-blox ag (1999). Datum Transformations of GPS Positions.
     Application Note.
 """
-function ecef_to_geodetic(r_e::AbstractVector)
+function ecef_to_geodetic(r_e::AbstractVector; ellipsoid = wgs84_ellipsoid)
     # Auxiliary variables.
     X = r_e[1]
     Y = r_e[2]
     Z = r_e[3]
 
     # Auxiliary variables.
+	a = ellipsoid.a
+	b = ellipsoid.b
+	e² = ellipsoid.e²
+	el² = ellipsoid.el²
     p = √(X^2 + Y^2)
-    θ = atan(Z * a_wgs84, p * b_wgs84)
-    e_wgs84² = e_wgs84^2
+    θ = atan(Z * a, p * b)
     sin_θ, cos_θ = sincos(θ)
 
     # Compute Geodetic.
     lon = atan(Y, X)
     lat = atan(
-        Z + el_wgs84^2 * b_wgs84 * sin_θ^3,
-        p -   e_wgs84² * a_wgs84 * cos_θ^3
+        Z + el² * b * sin_θ^3,
+        p -  e² * a * cos_θ^3
     )
 
     sin_lat, cos_lat = sincos(lat)
 
-    N = a_wgs84/√(1 - e_wgs84² * sin_lat^2)
+    N = a/√(1 - e² * sin_lat^2)
 
     # Avoid singularity if we are near the poles (~ 1 deg according to [1,
     # p.172]). Note that `cosd(1) = -0.01745240643728351`.
     if !(-0.01745240643728351 < cos_lat < 0.01745240643728351)
         h = p/cos_lat - N
     else
-        h = Z/sin_lat - N*(1 - e_wgs84²)
+        h = Z/sin_lat - N*(1 - e²)
     end
 
     return lat, lon, h
 end
 
 """
-    geodetic_to_ecef(lat::Number, lon::Number, h::Number)
+    geodetic_to_ecef(lat::Number, lon::Number, h::Number; ellipsoid=wgs84_ellipsoid)
 
-Convert the latitude `lat` [rad], longitude `lon` [rad], and altitude `h` \\[m]
-(WGS-84) into a vector represented on the Earth-Centered, Earth-Fixed (ECEF)
-reference frame.
+Convert the latitude `lat` [rad], longitude `lon` [rad], and altitude `h` \\[m] above the 
+reference ellipsoid (defaults to WGS-84) into a vector represented on the Earth-Centered, 
+Earth-Fixed (ECEF) reference frame.
 
 !!! info
     The algorithm is based in **[3]**.
@@ -99,28 +102,31 @@ reference frame.
 - **[3]**: mu-blox ag (1999). Datum Transformations of GPS Positions.
     Application Note.
 """
-function geodetic_to_ecef(lat::Number, lon::Number, h::Number)
+function geodetic_to_ecef(lat::Number, lon::Number, h::Number; ellipsoid = wgs84_ellipsoid)
     # Auxiliary variables.
     sin_lat, cos_lat = sincos(lat)
     sin_lon, cos_lon = sincos(lon)
+	a = ellipsoid.a
+	b = ellipsoid.b
+	e² = ellipsoid.e²
 
     # Radius of curvature [m].
-    N = a_wgs84/√(1 - e_wgs84^2 * sin_lat^2)
+    N = a/√(1 - e² * sin_lat^2)
 
     # Compute the position in ECEF frame.
     return SVector(
         (                        N + h) * cos_lat * cos_lon,
         (                        N + h) * cos_lat * sin_lon,
-        ((b_wgs84 / a_wgs84)^2 * N + h) * sin_lat
+        (						(b / a)^2 * N + h) * sin_lat
     )
 end
 
 """
-    geocentric_to_geodetic(ϕ_gc::Number, r::Number)
+    geocentric_to_geodetic(ϕ_gc::Number, r::Number; ellipsoid = wgs84_ellipsoid)
 
-Compute the geodetic latitude and altitude (WGS-84) from the geocentric latitude
-`ϕ_gc` (-π/2, π/2) [rad] and radius `r` [m]. Notice that the longitude is the
-same in both geocentric and geodetic coordinates.
+Compute the geodetic latitude and altitude above the reference ellipsoid (defaults to WGS-84)
+from the geocentric latitude `ϕ_gc` (-π/2, π/2) [rad] and radius `r` [m]. 
+Notice that the longitude is the same in both geocentric and geodetic coordinates.
 
 !!! info
     The algorithm is based in **[5]**.
@@ -128,7 +134,7 @@ same in both geocentric and geodetic coordinates.
 # Returns
 
 * Geodetic latitude [rad].
-* Altitude above the reference ellipsoid (WGS-84) [m].
+* Altitude above the reference ellipsoid (defaults to WGS-84) [m].
 
 # References
 
@@ -136,7 +142,7 @@ same in both geocentric and geodetic coordinates.
     coordinates without approximations. Astrophysics and Space Science, vol.
     139, pp. 1-4.
 """
-function geocentric_to_geodetic(ϕ_gc::Number, r::Number)
+function geocentric_to_geodetic(ϕ_gc::Number, r::Number; ellipsoid = wgs84_ellipsoid)
     # Obtain the `z` component and the equatorial component `re`.
     sin_ϕ_gc, cos_ϕ_gc = sincos(ϕ_gc)
     re = r * cos_ϕ_gc
@@ -144,9 +150,9 @@ function geocentric_to_geodetic(ϕ_gc::Number, r::Number)
     sign_z = z == 0 ? +1 : sign(z)
 
     # Auxiliary variables.
-    a  = a_wgs84
+    a  = ellipsoid.a
     a² = a^2
-    b  = sign_z * b_wgs84
+    b  = sign_z * ellipsoid.b
     b² = b^2
 
     # Compute the parameters.
@@ -184,10 +190,10 @@ function geocentric_to_geodetic(ϕ_gc::Number, r::Number)
 end
 
 """
-    geodetic_to_geocentric(ϕ_gd::Number, h::Number)
+    geodetic_to_geocentric(ϕ_gd::Number, h::Number; ellipsoid = wgs84_ellipsoid)
 
 Compute the geocentric latitude and radius from the geodetic latitude `ϕ_gd`
-(-π/2, π/2) [rad] and height above the reference ellipsoid `h` \\[m] (WGS-84).
+(-π/2, π/2) [rad] and height above the reference ellipsoid `h` \\[m] (defaults to WGS-84).
 Notice that the longitude is the same in both geocentric and geodetic
 coordinates.
 
@@ -203,18 +209,19 @@ coordinates.
 
 - **[4]** ISO TC 20/SC 14 N (2011). Geomagnetic Reference Models.
 """
-function geodetic_to_geocentric(ϕ_gd::Number, h::Number)
+function geodetic_to_geocentric(ϕ_gd::Number, h::Number; ellipsoid = wgs84_ellipsoid)
     # Auxiliary variables to decrease computational burden.
     sin_ϕ_gd, cos_ϕ_gd = sincos(ϕ_gd)
     sin²_ϕ_gd = sin_ϕ_gd^2
-    e²_wgs84  = e_wgs84^2
+    a  = ellipsoid.a
+    e²  = ellipsoid.e²
 
     # Radius of curvature in the prime vertical [m].
-    N    = a_wgs84/√(1 - e²_wgs84 * sin²_ϕ_gd )
+    N    = a/√(1 - e² * sin²_ϕ_gd )
 
     # Compute the geocentric latitude and radius from the Earth center.
     ρ    = (N + h) * cos_ϕ_gd
-    z    = (N * (1 - e²_wgs84) + h) * sin_ϕ_gd
+    z    = (N * (1 - e²) + h) * sin_ϕ_gd
     r    = √(ρ^2 + z^2)
     ϕ_gc = asin(z / r)
 
