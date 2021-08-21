acd108ac0e91d0aa905777b4dd1fc10bf9892291
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-08-21 18:30:07 -0300

    :art: Apply BlueStyle to s/m/dlegendre.jl

--- src/misc/dlegendre.jl
+++ src/misc/dlegendre.jl
@@ -1,6 +1,7 @@
-#== # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Description
+# ==============================================================================
 #
 #   Functions related to the first-order derivative of associated Legendre
 #   functions.
@@ -8,6 +9,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # References
+# ==============================================================================
 #
 #   [1] Du, J., Chen, C., Lesur, V., and Wang, L (2015). Non-singular spherical
 #       harmonic expressions of geomagnetic vector and gradient tensor fields in
@@ -18,7 +20,7 @@
 #       Körper-Gravitationswechselwirkung, Deutsche Geodätische Kommission.
 #       Reihe C, Heft Nr. 288, München, 1983.
 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ==#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 export dlegendre!, dlegendre
 export dlegendre_fully_normalized!, dlegendre_fully_normalized
@@ -46,34 +48,64 @@ computed using the same normalization (see `N`) as the one selected here.
 The optional parameter `N` can be used to select the normalization. The
 following values are valid:
 
-* `Val(:full)`: Compute the fully normalized associated Legendre function (see
-                `dlegendre_fully_normalized!`).
-* `Val(:schmidt)`: Compute the Schmidt quasi-normalized associated Legendre
-                   function (see `dlegendre_schmidt_quasi_normalized!`).
-* `Val(:conv)`: Compute the conventional associated Legendre function (see
-                `dlegendre_conventional!`).
+- `Val(:full)`: Compute the fully normalized associated Legendre function (see
+    [`dlegendre_fully_normalized!`](@ref)).
+- `Val(:schmidt)`: Compute the Schmidt quasi-normalized associated Legendre
+    function (see [`dlegendre_schmidt_quasi_normalized!`](@ref)).
+- `Val(:conv)`: Compute the conventional associated Legendre function (see
+    [`dlegendre_conventional!`](@ref)).
 
 If `N` is omitted, then the full normalization will be used (`Val(:full)`).
 
 If `ph_term` is set to `true`, then the Condon-Shortley phase term `(-1)ᵐ` will
 be included. If `ph_term` is not present, then it defaults to `false`.
-
 """
-dlegendre!(dP::AbstractMatrix, ϕ::Number, P::AbstractMatrix,
-           ph_term::Bool = false, n_max::Integer = -1, m_max::Integer = -1) =
-    dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+function dlegendre!(
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
+    return dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+end
 
-dlegendre!(::Val{:full}, dP::AbstractMatrix, ϕ::Number, P::AbstractMatrix,
-           ph_term::Bool = false, n_max::Integer = -1, m_max::Integer = -1) =
-    dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+function dlegendre!(
+    ::Val{:full},
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
+    return dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+end
 
-dlegendre!(::Val{:schmidt}, dP::AbstractMatrix, ϕ::Number, P::AbstractMatrix,
-           ph_term::Bool = false, n_max::Integer = -1, m_max::Integer = -1) =
-    dlegendre_schmidt_quasi_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+function dlegendre!(
+    ::Val{:schmidt},
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
+    return dlegendre_schmidt_quasi_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+end
 
-dlegendre!(::Val{:conv}, dP::AbstractMatrix, ϕ::Number, P::AbstractMatrix,
-           ph_term::Bool = false, n_max::Integer = -1, m_max::Integer = -1) =
-    dlegendre_conventional!(dP, ϕ, P, ph_term, n_max, m_max)
+function dlegendre!(
+    ::Val{:conv},
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
+    return dlegendre_conventional!(dP, ϕ, P, ph_term, n_max, m_max)
+end
 
 """
     dlegendre([N,] ϕ::Number, n_max::Integer, m_max::Integer = -1, ph_term::Bool = false)
@@ -92,12 +124,12 @@ set to `n_max`.
 The optional parameter `N` can be used to select the normalization. The
 following values are valid:
 
-* `Val(:full)`: Compute the fully normalized associated Legendre function (see
-                `legendre_fully_normalized`).
-* `Val(:schmidt)`: Compute the Schmidt quasi-normalized associated Legendre
-                   function (see `legendre_schmidt_quasi_normalized`).
-* `Val(:conv)`: Compute the conventional associated Legendre function (see
-                `dlegendre_conventional!`).
+- `Val(:full)`: Compute the fully normalized associated Legendre function (see
+    [`legendre_fully_normalized`](@ref)).
+- `Val(:schmidt)`: Compute the Schmidt quasi-normalized associated Legendre
+    function (see [`legendre_schmidt_quasi_normalized`](@ref)).
+- `Val(:conv)`: Compute the conventional associated Legendre function (see
+    [`dlegendre_conventional!`](@ref)).
 
 If `N` is omitted, then the full normalization will be used (`Val(:full)`).
 
@@ -106,25 +138,48 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 
 # Returns
 
-* A matrix with the first-order derivative of the Legendre associated functions
-`P_n,m[cos(ϕ)]`.
-* A matrix with the Legendre associated functions `P_n,m[cos(ϕ)]`.
-
+- A matrix with the first-order derivative of the Legendre associated functions
+    `P_n,m[cos(ϕ)]`.
+- A matrix with the Legendre associated functions `P_n,m[cos(ϕ)]`.
 """
-dlegendre(ϕ::Number, n_max::Integer, m_max::Integer = -1, ph_term::Bool = false) =
-    dlegendre_fully_normalized(float(ϕ), n_max, m_max, ph_term)
+function dlegendre(
+    ϕ::Number,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+)
+    return dlegendre_fully_normalized(float(ϕ), n_max, m_max, ph_term)
+end
 
-dlegendre(::Val{:full}, ϕ::Number, n_max::Integer, m_max::Integer = -1,
-          ph_term::Bool = false) =
-    dlegendre_fully_normalized(float(ϕ), n_max, m_max, ph_term)
+function dlegendre(
+    ::Val{:full},
+    ϕ::Number,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+)
+    return dlegendre_fully_normalized(float(ϕ), n_max, m_max, ph_term)
+end
 
-dlegendre(::Val{:schmidt}, ϕ::Number, n_max::Integer, m_max::Integer = -1,
-          ph_term::Bool = false) =
-    dlegendre_schmidt_quasi_normalized(float(ϕ), n_max, m_max, ph_term)
+function dlegendre(
+    ::Val{:schmidt},
+    ϕ::Number,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+)
+    return dlegendre_schmidt_quasi_normalized(float(ϕ), n_max, m_max, ph_term)
+end
 
-dlegendre(::Val{:conv}, ϕ::Number, n_max::Integer, m_max::Integer = -1,
-          ph_term::Bool = false) =
-    dlegendre_conventional(float(ϕ), n_max, m_max, ph_term)
+function dlegendre(
+    ::Val{:conv},
+    ϕ::Number,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+)
+    return dlegendre_conventional(float(ϕ), n_max, m_max, ph_term)
+end
 
 ################################################################################
 #                Fully Normalized Associated Legendre Functions
@@ -156,12 +211,15 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 The user is responsible to pass a matrix `P` with the correct values. For
 example, if `ph_term` is `true`, then `P` must also be computed with `ph_term`
 set to `true`.
-
 """
-function dlegendre_fully_normalized!(dP::AbstractMatrix, ϕ::Number,
-                                     P::AbstractMatrix, ph_term::Bool = false,
-                                     n_max::Integer = -1, m_max::Integer = -1)
-
+function dlegendre_fully_normalized!(
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
     # Obtain the maximum degree and order that must be computed.
     n_max, m_max = _get_degree_and_order(dP, P, n_max, m_max)
 
@@ -197,20 +255,22 @@ function dlegendre_fully_normalized!(dP::AbstractMatrix, ϕ::Number,
     # In fact, in [2, p. 119], it is mentioned that `0 <= ϕ <= π`.  However,
     # further clarification is required.
 
-    ϕ    = mod(ϕ,2*π)
+    ϕ    = mod(ϕ, 2π)
     fact = (ϕ > π) ? -1 : 1
 
-    ph_term && (fact *= -1)
+    if ph_term
+        fact *= -1
+    end
 
     dP[0+1,0+1] = 0
 
     m_max < 0 && return nothing
 
-    @inbounds for n = 1:n_max
-        for m = 0:n
+    @inbounds for n in 1:n_max
+        for m in 0:n
             if m == 0
-                aux  = sqrt(n*(n+1)/2)
-                a_nm = aux/2
+                aux  = sqrt(n * (n + 1) / 2)
+                a_nm = aux / 2
                 b_nm = -a_nm
 
                 # Notice that [1, p. 1985]:
@@ -218,30 +278,30 @@ function dlegendre_fully_normalized!(dP::AbstractMatrix, ϕ::Number,
                 #                  m
                 #   P_(n,-m) = (-1).P_(n,m) ,
                 #
-                dP[n+1,0+1] = -a_nm*P[n+1,1+1] + b_nm*P[n+1,0+1+1]
+                dP[n+1,0+1] = -a_nm * P[n+1,1+1] + b_nm * P[n+1,0+1+1]
 
             # We should consider the case `m == 1` separately from `n == m`
             # because of the coefficient `C_{m}`.
             elseif m == 1
-                a_nm = sqrt(2n*(n+1))/2
-                dP[n+1,1+1] = a_nm*P[n+1,1-1+1]
+                a_nm = sqrt(2n * (n + 1)) / 2
+                dP[n+1,1+1] = a_nm * P[n+1,1-1+1]
 
                 # Only compute `b_nm` if `n > 1`. Otherwise, we could access an
                 # invalid memory region if `P` is 2x2.
                 if n > 1
-                    b_nm = -sqrt((n+2)*(n-1))/2
-                    dP[n+1,1+1] += b_nm*P[n+1,1+1+1]
+                    b_nm = -sqrt((n + 2) * (n - 1)) / 2
+                    dP[n+1,1+1] += b_nm * P[n+1,1+1+1]
                 end
 
             elseif n != m
-                a_nm = +sqrt((n+m)*(n-m+1))/2
-                b_nm = -sqrt((n+m+1)*(n-m))/2
+                a_nm = +sqrt((n + m) * (n - m + 1)) / 2
+                b_nm = -sqrt((n + m + 1) * (n - m)) / 2
 
-                dP[n+1,m+1] = a_nm*P[n+1,m-1+1] + b_nm*P[n+1,m+1+1]
+                dP[n+1,m+1] = a_nm * P[n+1,m-1+1] + b_nm * P[n+1,m+1+1]
             else
-                a_nm = +sqrt((n+m)*(n-m+1))/2
+                a_nm = +sqrt((n + m) * (n - m + 1)) / 2
 
-                dP[n+1,m+1] = a_nm*P[n+1,m-1+1]
+                dP[n+1,m+1] = a_nm * P[n+1,m-1+1]
             end
 
             dP[n+1,m+1] *= fact
@@ -275,12 +335,16 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 
 A matrix with the first-order derivative of the Legendre associated functions
 `P_n,m[cos(ϕ)]`.
-
 """
-function dlegendre_fully_normalized(ϕ::T, n_max::Integer, m_max::Integer = -1,
-                                    ph_term::Bool = false) where T<:AbstractFloat
-
-    ( (m_max < 0) || (m_max > n_max) ) && (m_max = n_max)
+function dlegendre_fully_normalized(
+    ϕ::T,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+) where T<:AbstractFloat
+    if ((m_max < 0) || (m_max > n_max))
+        m_max = n_max
+    end
 
     # Check if we need to compute and additional degree in `P` to provide the
     # desire order in `dP`.
@@ -298,6 +362,7 @@ function dlegendre_fully_normalized(ϕ::T, n_max::Integer, m_max::Integer = -1,
     # functions.
     dP = zeros(T, n_max + 1, m_max + 1)
     dlegendre_fully_normalized!(dP, ϕ, P, ph_term)
+
     return dP, P
 end
 
@@ -331,16 +396,20 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 The user is responsible to pass a matrix `P` with the correct values. For
 example, if `ph_term` is `true`, then `P` must also be computed with `ph_term`
 set to `true`.
-
 """
-dlegendre_schmidt_quasi_normalized!(dP::AbstractMatrix, ϕ::Number,
-                                    P::AbstractMatrix, ph_term::Bool = false,
-                                    n_max::Integer = -1, m_max::Integer = -1) =
-
+function dlegendre_schmidt_quasi_normalized!(
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
     # The algorithm to compute the first-order derivative using Schmidt
     # normalization is precisely the same as the one that computes using full
     # normalization.
-    dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+    return dlegendre_fully_normalized!(dP, ϕ, P, ph_term, n_max, m_max)
+end
 
 """
     dlegendre_schmidt_quasi_normalized(ϕ::T, n_max::Integer, m_max::Integer = -1, ph_term::Bool = false) where T<:AbstractFloat
@@ -365,11 +434,15 @@ A matrix with the first-order derivative of the Legendre associated functions
 `P_n,m[cos(ϕ)]`.
 
 """
-function dlegendre_schmidt_quasi_normalized(ϕ::T, n_max::Integer,
-                                            m_max::Integer = -1,
-                                            ph_term::Bool = false) where T<:AbstractFloat
-
-    ( (m_max < 0) || (m_max > n_max) ) && (m_max = n_max)
+function dlegendre_schmidt_quasi_normalized(
+    ϕ::T,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+) where T<:AbstractFloat
+    if ((m_max < 0) || (m_max > n_max))
+        m_max = n_max
+    end
 
     # Check if we need to compute and additional degree in `P` to provide the
     # desire order in `dP`.
@@ -387,6 +460,7 @@ function dlegendre_schmidt_quasi_normalized(ϕ::T, n_max::Integer,
     # functions.
     dP = zeros(T, n_max + 1, m_max + 1)
     dlegendre_schmidt_quasi_normalized!(dP, ϕ, P, ph_term)
+
     return dP, P
 end
 
@@ -419,12 +493,15 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 The user is responsible to pass a matrix `P` with the correct values. For
 example, if `ph_term` is `true`, then `P` must also be computed with `ph_term`
 set to `true`.
-
 """
-function dlegendre_conventional!(dP::AbstractMatrix, ϕ::Number, P::AbstractMatrix,
-                                 ph_term::Bool = false, n_max::Integer = -1,
-                                 m_max::Integer = -1)
-
+function dlegendre_conventional!(
+    dP::AbstractMatrix,
+    ϕ::Number,
+    P::AbstractMatrix,
+    ph_term::Bool = false,
+    n_max::Integer = -1,
+    m_max::Integer = -1
+)
     # Obtain the maximum degree and order that must be computed.
     n_max, m_max = _get_degree_and_order(dP, P, n_max, m_max)
 
@@ -444,7 +521,7 @@ function dlegendre_conventional!(dP::AbstractMatrix, ϕ::Number, P::AbstractMatr
     # In fact, in [2, p. 119], it is mentioned that `0 <= ϕ <= π`.  However,
     # further clarification is required.
 
-    ϕ    = mod(ϕ,2*π)
+    ϕ    = mod(ϕ, 2π)
     fact = (ϕ > π) ? -1 : 1
 
     ph_term && (fact *= -1)
@@ -494,12 +571,16 @@ be included. If `ph_term` is not present, then it defaults to `false`.
 
 A matrix with the first-order derivative of the Legendre associated functions
 `P_n,m[cos(ϕ)]`.
-
 """
-function dlegendre_conventional(ϕ::T, n_max::Integer, m_max::Integer = -1,
-                                ph_term::Bool = false) where T<:AbstractFloat
-
-    ( (m_max < 0) || (m_max > n_max) ) && (m_max = n_max)
+function dlegendre_conventional(
+    ϕ::T,
+    n_max::Integer,
+    m_max::Integer = -1,
+    ph_term::Bool = false
+) where T<:AbstractFloat
+    if ((m_max < 0) || (m_max > n_max))
+        m_max = n_max
+    end
 
     # Check if we need to compute and additional degree in `P` to provide the
     # desire order in `dP`.
@@ -517,6 +598,7 @@ function dlegendre_conventional(ϕ::T, n_max::Integer, m_max::Integer = -1,
     # functions.
     dP = zeros(T, n_max + 1, m_max + 1)
     dlegendre_conventional!(dP, ϕ, P, ph_term)
+
     return dP, P
 end
 
@@ -533,20 +615,30 @@ given the matrices `dP`, `P`, and the configuration values `n_max` and `m_max`.
 """
 @inline function _get_degree_and_order(dP, P, n_max, m_max)
     # Get the size of the matrices.
-    (Prows,  Pcols)  = size(P)
-    (dProws, dPcols) = size(dP)
+    Prows,  Pcols  = size(P)
+    dProws, dPcols = size(dP)
 
     rows = min(Prows, dProws)
     cols = min(Pcols, dPcols)
 
     # If the order or degree is less than 0, then the user wants to use all the
     # available memory.
-    n_max < 0 && (n_max = rows - 1)
-    m_max < 0 && (m_max = cols <= rows ? cols - 1 : n_max)
+    if n_max < 0
+        n_max = rows - 1
+    end
+
+    if m_max < 0
+        m_max = (cols <= rows) ? cols - 1 : n_max
+    end
 
     # Make sure that the degree and order fits the matrix.
-    n_max > rows - 1 && (n_max = rows - 1)
-    ( (m_max > cols - 1) || (m_max > n_max) ) && (m_max = min(cols-1, n_max))
+    if n_max > rows - 1
+        n_max = rows - 1
+    end
+
+    if ((m_max > cols - 1) || (m_max > n_max))
+        m_max = min(cols - 1, n_max)
+    end
 
     return n_max, m_max
 end
