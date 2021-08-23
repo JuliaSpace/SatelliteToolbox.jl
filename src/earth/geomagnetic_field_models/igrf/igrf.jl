490e4f134cdf26dcaa17cbd8c234c2069da66b27
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-08-23 14:43:12 -0300

    :art: Apply BlueStyle to s/e/g/i/igrf.jl

--- src/earth/geomagnetic_field_models/igrf/igrf.jl
+++ src/earth/geomagnetic_field_models/igrf/igrf.jl
@@ -1,18 +1,20 @@
-#== # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Description
+# ==============================================================================
 #
 #   International Geomagnetic Field Model.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # References
+# ==============================================================================
 #
 #   [1] https://www.ngdc.noaa.gov/IAGA/vmod/igrf.html
 #   [2] https://www.ngdc.noaa.gov/IAGA/vmod/igrf12.f
 #   [3] https://www.mathworks.com/matlabcentral/fileexchange/34388-international-geomagnetic-reference-field--igrf--model
 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ==#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 export igrf, igrfd
 
@@ -34,8 +36,8 @@ The position representation is defined by `T`. If `T` is `Val(:geocentric)`,
 then the input must be **geocentric** coordinates:
 
 1. Distance from the Earth center `r` [m];
-1. Geocentric latitude `λ` (-90°, +90°); and
-2. Geocentric longitude `Ω` (-180°, +180°).
+2. Geocentric latitude `λ` (-90°, +90°); and
+3. Geocentric longitude `Ω` (-180°, +180°).
 
 If `T` is `Val(:geodetic)`, then the input must be **geodetic** coordinates:
 
@@ -53,15 +55,19 @@ coordinate system. In case of **geodetic coordinates**, the X-axis is tangent to
 the ellipsoid at the selected location and points toward North, whereas the
 Z-axis completes a right-hand coordinate system.
 
-The optional arguments `P` and `dP` must be two matrices with at least 14x14
-real numbers. If they are present, then they will be used to store the Legendre
-coefficients and their derivatives. In this case, no allocation will be
-performed when computing the magnetic field. If they are not present, then 2
-allocations will happen to create them.
+The optional arguments `P` and `dP` must be two matrices with at least
+`max_degree + 1 × max_degree + 1` real numbers. If they are present, then they
+will be used to store the Legendre coefficients and their derivatives. In this
+case, no allocation will be performed when computing the magnetic field. If they
+are not present, then 2 allocations will happen to create them.
 
 # Keywords
 
-* `show_warns`: Show warnings about the data (**Default** = `true`).
+* `max_degree::Int`: Maximum degree used in the spherical harmonics when
+    computing the geomagnetic field. If it is higher than the available number
+    of coefficients in the IGRF matrices, then it will be clamped.
+    (**Default** = 13)
+* `show_warns::Bool`: Show warnings about the data (**Default** = `true`).
 
 # Remarks
 
@@ -75,30 +81,52 @@ more readable code than the original one in FORTRAN, because it uses features
 available in Julia language.
 
 """
-@inline igrfd(date::Number, r::Number, λ::Number, Ω::Number; show_warns = true) =
-    igrfd(date, r, λ, Ω, Val(:geocentric); show_warns = show_warns)
+@inline function igrfd(
+    date::Number,
+    r::Number,
+    λ::Number,
+    Ω::Number;
+    show_warns = true
+)
+    return igrfd(date, r, λ, Ω, Val(:geocentric); show_warns = show_warns)
+end
 
-@inline igrfd(date::Number, r::Number, λ::Number, Ω::Number,
-              P::AbstractMatrix{T}, dP::AbstractMatrix{T};
-              show_warns = true) where T<:Real =
-    igrfd(date, r, λ, Ω, Val(:geocentric), P, dP; show_warns = show_warns)
+@inline function igrfd(
+    date::Number,
+    r::Number,
+    λ::Number,
+    Ω::Number,
+    P::AbstractMatrix{T},
+    dP::AbstractMatrix{T};
+    show_warns = true
+) where T<:Real
+    return igrfd(date, r, λ, Ω, Val(:geocentric), P, dP; show_warns = show_warns)
+end
 
-@inline function igrfd(date::Number, rh::Number, λ::Number, Ω::Number, R::T;
-                       show_warns = true) where {T<:Union{Val{:geocentric},
-                                                          Val{:geodetic}}}
-
+@inline function igrfd(
+    date::Number,
+    rh::Number,
+    λ::Number,
+    Ω::Number,
+    R::T;
+    show_warns = true
+) where {T<:Union{Val{:geocentric}, Val{:geodetic}}}
     P  = Matrix{Float64}(undef, 14, 14)
     dP = Matrix{Float64}(undef, 14, 14)
 
     return igrfd(date, rh, λ, Ω, R, P, dP; show_warns = show_warns)
 end
 
-@inline function igrfd(date::Number, rh::Number, λ::Number, Ω::Number, R::T,
-                       P::AbstractMatrix{S}, dP::AbstractMatrix{S};
-                       show_warns = true) where {T<:Union{Val{:geocentric},
-                                                          Val{:geodetic}},
-                                                 S<:Real}
-
+@inline function igrfd(
+    date::Number,
+    rh::Number,
+    λ::Number,
+    Ω::Number,
+    R::T,
+    P::AbstractMatrix{S},
+    dP::AbstractMatrix{S};
+    show_warns = true
+) where {T<:Union{Val{:geocentric}, Val{:geodetic}}, S<:Real}
     # Check if the latitude and longitude are valid.
     ( (λ < -90) || (λ > 90) ) &&
     error("The latitude must be between -90° and +90° rad.")
@@ -123,12 +151,12 @@ The position representation is defined by `T`. If `T` is `Val(:geocentric)`,
 then the input must be **geocentric** coordinates:
 
 1. Distance from the Earth center `r` [m];
-1. Geocentric latitude `λ` (-π/2, +π/2) \\[rad]; and
-2. Geocentric longitude `Ω` (-π, +π) \\[rad].
+2. Geocentric latitude `λ` (-π/2, +π/2) \\[rad]; and
+3. Geocentric longitude `Ω` (-π, +π) \\[rad].
 
 If `T` is `Val(:geodetic)`, then the input must be **geodetic** coordinates:
 
-1 Altitude above the reference ellipsoid `h` (WGS-84) \\[m];
+1. Altitude above the reference ellipsoid `h` (WGS-84) \\[m];
 2. Geodetic latitude `λ` (-π/2, +π/2) \\[rad]; and
 3. Geodetic longitude `Ω` (-π, +π) \\[rad].
 
@@ -164,17 +192,30 @@ more readable code than the original one in FORTRAN, because it uses features
 available in Julia language.
 
 """
-igrf(date::Number, r::Number, λ::Number, Ω::Number; show_warns = true) =
-    igrf(date, r, λ, Ω, Val(:geocentric); show_warns = show_warns)
+function igrf(date::Number, r::Number, λ::Number, Ω::Number; show_warns = true)
+    return igrf(date, r, λ, Ω, Val(:geocentric); show_warns = show_warns)
+end
 
-igrf(date::Number, r::Number, λ::Number, Ω::Number, P::AbstractMatrix{T},
-     dP::AbstractMatrix{T}; show_warns = true) where T<:Real =
-    igrf(date, r, λ, Ω, Val(:geocentric), P, dP; show_warns = show_warns)
+function igrf(
+    date::Number,
+    r::Number,
+    λ::Number,
+    Ω::Number,
+    P::AbstractMatrix{T},
+    dP::AbstractMatrix{T};
+    show_warns = true
+) where T<:Real
+    return igrf(date, r, λ, Ω, Val(:geocentric), P, dP; show_warns = show_warns)
+end
 
-function igrf(date::Number, r::Number, λ::Number, Ω::Number, R::T;
-              show_warns::Bool = true) where {T<:Union{Val{:geocentric},
-                                                       Val{:geodetic}}}
-
+function igrf(
+    date::Number,
+    r::Number,
+    λ::Number,
+    Ω::Number,
+    R::T;
+    show_warns::Bool = true
+) where {T<:Union{Val{:geocentric}, Val{:geodetic}}}
     P  = Matrix{Float64}(undef, 14, 14)
     dP = Matrix{Float64}(undef, 14, 14)
 
@@ -197,26 +238,30 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
 
     # Check the data, since this model is valid for years between 1900 and
     # `max_year`.
-    ( (date < 1900) || (date > max_year) ) &&
-    error("This IGRF version will not work for years outside the interval [1900, $max_year).")
+    if ((date < 1900) || (date > max_year))
+        error("This IGRF version will not work for years outside the interval [1900, $max_year).")
+    end
 
     # Check if the latitude and longitude are valid.
-    ( (λ < -pi/2) || (λ > pi/2) ) &&
-    error("The latitude must be between -π/2 and +π/2 rad.")
+    if ((λ < -π/2) || (λ > π/2))
+        error("The latitude must be between -π/2 and +π/2 rad.")
+    end
 
-    ( (Ω < -pi) || (Ω > pi) ) &&
-    error("The longitude must be between -π and +π rad.")
+    if ((Ω < -π) || (Ω > π))
+        error("The longitude must be between -π and +π rad.")
+    end
 
     # Warn the user that for dates after the year `rel_year` the accuracy maybe
     # reduced.
-    show_warns && (date > rel_year) &&
-    @warn("The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than $rel_year.")
+    if show_warns && (date > rel_year)
+        @warn("The magnetic field computed with this IGRF version may be of reduced accuracy for years greater than $rel_year.")
+    end
 
     # Input variables conversion
     # ==========================
 
     # Convert latitude / longitude to colatitude and east-longitude.
-    θ = pi/2 - λ
+    θ = π / 2 - λ
     ϕ = (Ω >= 0) ? Ω : 2pi + Ω
 
     # The input variable `r` is in [m], but all the algorithm requires it to be
@@ -229,8 +274,8 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
     # Compute the epoch that will be used to obtain the coefficients. This is
     # necessary because the IGRF provides coefficients every 5 years. Between
     # two epochs, those coefficients must be interpolated.
-    idx   = floor(Int, clamp((date-1900)*0.2+1, 0, (rel_year-1900)/5))
-    epoch = 1900 + (idx-1)*5
+    idx   = floor(Int, clamp((date - 1900) * 0.2 + 1, 0, (rel_year - 1900) / 5))
+    epoch = 1900 + (idx - 1) * 5
 
     # Compute the fraction of time from the epoch of the coefficient selected by
     # `idx`.
@@ -241,10 +286,15 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
 
     # Make sure we have the required amount of space in the matrices.
     (rows, cols) = size(P)
-    ((rows < n_max+1) || (cols < n_max+1)) && error("Matrix `P` must have at least $(n_max+1) rows and columns.")
 
-    (rows, cols) = size(dP)
-    ((rows < n_max+1) || (cols < n_max+1)) && error("Matrix `dP` must have at least $(n_max+1) rows and columns.")
+    if (rows < n_max + 1) || (cols < n_max + 1)
+        error("Matrix `P` must have at least $(n_max + 1) rows and columns.")
+    end
+
+    rows, cols = size(dP)
+    if ((rows < n_max + 1) || (cols < n_max + 1))
+        error("Matrix `dP` must have at least $(n_max+1) rows and columns.")
+    end
 
     # Compute the Schmidt quasi-normalized associated Legendre functions and
     # their first order derivative, neglecting the phase term.
@@ -271,8 +321,9 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
     # Auxiliary variables to decrease the computational burden.
     sin_ϕ,  cos_ϕ  = sincos(1ϕ)
     sin_2ϕ, cos_2ϕ = sincos(2ϕ)
-    ratio   = a/r
-    fact    = ratio
+
+    ratio = a/r
+    fact  = ratio
 
     # Initialization of variables
     # ===========================
@@ -288,7 +339,7 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
     # Geomagnetic potential
     # =====================
 
-    @inbounds for n = 1:n_max
+    @inbounds for n in 1:n_max
         aux_dVr = 0.0
         aux_dVθ = 0.0
         aux_dVϕ = 0.0
@@ -302,16 +353,16 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
 
         if date < dat_year
             Gnm_e1 = G[kg,idx+3]
-            ΔG     = (Gnm_e1-Gnm_e0)/5
+            ΔG     = (Gnm_e1 - Gnm_e0) / 5
         else
             ΔG     = G[kg,gend]
         end
 
-        Gnm  = Gnm_e0 + ΔG*Δt
+        Gnm  = Gnm_e0 + ΔG * Δt
         kg  += 1
 
-        aux_dVr += -(n+1)/r*Gnm*P[n+1,1]
-        aux_dVθ += Gnm*dP[n+1,1]
+        aux_dVr += -(n + 1) / r * Gnm * P[n+1,1]
+        aux_dVθ += Gnm * dP[n+1,1]
 
         # Sine and cosine with m = 1
         # ==========================
@@ -335,8 +386,8 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
 
         for m = 1:n
             # Compute recursively `sin(m*ϕ)` and `cos(m*ϕ)`.
-            sin_mϕ = 2cos_ϕ*sin_m_1ϕ-sin_m_2ϕ
-            cos_mϕ = 2cos_ϕ*cos_m_1ϕ-cos_m_2ϕ
+            sin_mϕ = 2cos_ϕ * sin_m_1ϕ - sin_m_2ϕ
+            cos_mϕ = 2cos_ϕ * cos_m_1ϕ - cos_m_2ϕ
 
             # Compute the coefficients `G_nm` and `H_nm`
             # ==========================================
@@ -349,27 +400,29 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
             if date < dat_year
                 Gnm_e1 = G[kg,idx+3]
                 Hnm_e1 = H[kh,idx+3]
-                ΔG     = (Gnm_e1-Gnm_e0)/5
-                ΔH     = (Hnm_e1-Hnm_e0)/5
+                ΔG     = (Gnm_e1 - Gnm_e0) / 5
+                ΔH     = (Hnm_e1 - Hnm_e0) / 5
             else
-                ΔG     = G[kg,gend]
-                ΔH     = H[kh,hend]
+                ΔG = G[kg,gend]
+                ΔH = H[kh,hend]
             end
 
-            Gnm    = Gnm_e0 + ΔG*Δt
-            Hnm    = Hnm_e0 + ΔH*Δt
-            kg    += 1
-            kh    += 1
+            Gnm  = Gnm_e0 + ΔG * Δt
+            Hnm  = Hnm_e0 + ΔH * Δt
+            kg  += 1
+            kh  += 1
 
-            GcHs_nm = Gnm*cos_mϕ + Hnm*sin_mϕ
-            GsHc_nm = Gnm*sin_mϕ - Hnm*cos_mϕ
+            GcHs_nm = Gnm * cos_mϕ + Hnm * sin_mϕ
+            GsHc_nm = Gnm * sin_mϕ - Hnm * cos_mϕ
 
             # Compute the contributions for `m`
             # =================================
 
-            aux_dVr += -fact_dVr*GcHs_nm*P[n+1,m+1]
-            aux_dVθ += GcHs_nm*dP[n+1,m+1]
-            aux_dVϕ += (θ == 0) ? -m*GsHc_nm*dP[n+1,m+1] : -m*GsHc_nm*P[n+1,m+1]
+            aux_dVr += -fact_dVr * GcHs_nm * P[n+1,m+1]
+            aux_dVθ += GcHs_nm * dP[n+1,m+1]
+            aux_dVϕ += (θ == 0) ?
+                -m * GsHc_nm * dP[n+1,m+1] :
+                -m * GsHc_nm * P[n+1,m+1]
 
             # Update the values for the next step
             # ===================================
@@ -384,7 +437,7 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
         # ==========================================================
 
         # fact = (a/r)^(n+1)
-        fact    *= ratio
+        fact *= ratio
 
         # aux_<> *= (a/r)^(n+1)
         aux_dVr *= fact
@@ -403,18 +456,25 @@ function igrf(date::Number, r::Number, λ::Number, Ω::Number, ::Val{:geocentric
     # Compute the Geomagnetic field vector in the geocentric reference frame
     # ======================================================================
 
-    x = +1/r*dVθ
-    y = (θ == 0) ? -1/r*dVϕ : -1/(r*sin(θ))*dVϕ
+    x = +dVθ / r
+    y = (θ == 0) ? -dVϕ / r : -dVϕ / (r * sin(θ))
     z = dVr
 
-    B_gc = SVector{3,Float64}(x,y,z)
+    B_gc = SVector{3,Float64}(x, y, z)
 
     return B_gc
 end
 
-function igrf(date::Number, h::Number, λ::Number, Ω::Number, ::Val{:geodetic},
-              P::AbstractMatrix{T}, dP::AbstractMatrix{T};
-              show_warns = true) where T<:Real
+function igrf(
+    date::Number,
+    h::Number,
+    λ::Number,
+    Ω::Number,
+    ::Val{:geodetic},
+    P::AbstractMatrix{T},
+    dP::AbstractMatrix{T};
+    show_warns = true
+) where T<:Real
 
     # TODO: This method has a small error (≈ 0.01 nT) compared with the
     # `igrf12syn`. However, the result is exactly the same as the MATLAB
@@ -423,15 +483,14 @@ function igrf(date::Number, h::Number, λ::Number, Ω::Number, ::Val{:geodetic},
     # caused by a numerical error. Further verification is necessary.
 
     # Convert the geodetic coordinates to geocentric coordinates.
-    (λ_gc, r) = geodetic_to_geocentric(λ, h)
+    λ_gc, r = geodetic_to_geocentric(λ, h)
 
     # Compute the geomagnetic field in geocentric coordinates.
     B_gc = igrf(date, r, λ_gc, Ω, Val(:geocentric), P, dP; show_warns = show_warns)
 
     # Convert to geodetic coordinates.
-    D_gd_gc = create_rotation_matrix(λ_gc - λ,:Y)
-    B_gd    = D_gd_gc*B_gc
+    D_gd_gc = create_rotation_matrix(λ_gc - λ, :Y)
+    B_gd    = D_gd_gc * B_gc
 
     return B_gd
 end
-
