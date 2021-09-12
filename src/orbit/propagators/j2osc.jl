365f05cad1118adf21619c12a7c6f3a819c0a823
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-09-12 20:05:10 -0300

    :wrench: Make J2 osc. propagator compatible with Float32

--- src/orbit/propagators/j2osc.jl
+++ src/orbit/propagators/j2osc.jl
@@ -30,38 +30,50 @@ export j2osc_init, j2osc!
 
 Initialize the data structure of J2 osculating orbit propagator algorithm.
 
+!!! note
+    The type used in the propagation will be the same as used to define the
+    gravitational constants in the structure `j2_gc`.
+
 # Args
 
-* `epoch`: Epoch of the orbital elements [Julian Day].
-* `a_0`: Initial semi-major axis [m].
-* `e_0`: Initial eccentricity.
-* `i_0`: Initial inclination [rad].
-* `Ω_0`: Initial right ascension of the ascending node [rad].
-* `ω_0`: Initial argument of perigee [rad].
-* `f_0`: Initial true anomaly [rad].
-* `dn_o2`: First time derivative of the mean motion divided by two [rad/s^2].
-* `ddn_o6`: Second time derivative of the mean motion divided by six [rad/s^3].
+- `epoch::Number`: Epoch of the initial mean orbital elements [Julian Day].
+- `a_0::Number`: Initial mean semi-major axis [m].
+- `e_0::Number`: Initial mean eccentricity.
+- `i_0::Number`: Initial mean inclination [rad].
+- `Ω_0::Number`: Initial mean right ascension of the ascending node [rad].
+- `ω_0::Number`: Initial mean argument of perigee [rad].
+- `f_0::Number`: Initial mean true anomaly [rad].
+- `dn_o2::Number`: First time derivative of the mean motion divided by two
+    [rad/s^2].
+- `ddn_o6::Number`: Second time derivative of the mean motion divided by six
+    [rad/s^3].
 
 # Keywords
 
-* `j2_gc`: J2 orbit propagator gravitational constants (see `J2_GravCte`).
-           (**Default** = `j2_gc_egm08`)
+- `j2_gc::J2_GravCte`: J2 orbit propagator gravitational constants (see
+    [`J2_GravCte`](@ref)). (**Default** = `j2_gc_egm08`)
 
 # Returns
 
-The structure `J2osc_Structure` with the initialized parameters.
+The structure [`J2osc_Structure`](@ref) with the initialized parameters.
 
 # Remarks
 
 The inputs are the mean orbital elements.
-
 """
-function j2osc_init(epoch::Number, a_0::Number, e_0::Number, i_0::Number,
-                    Ω_0::Number, ω_0::Number, f_0::Number, dn_o2::Number,
-                    ddn_o6::Number; j2_gc::J2_GravCte{T} = j2_gc_egm08) where T
-
-    j2d = j2_init(epoch, a_0, e_0, i_0, Ω_0, ω_0, f_0, dn_o2, ddn_o6;
-                  j2_gc = j2_gc)
+function j2osc_init(
+    epoch::Number,
+    a_0::Number,
+    e_0::Number,
+    i_0::Number,
+    Ω_0::Number,
+    ω_0::Number,
+    f_0::Number,
+    dn_o2::Number,
+    ddn_o6::Number;
+    j2_gc::J2_GravCte{T} = j2_gc_egm08
+) where T
+    j2d = j2_init(epoch, a_0, e_0, i_0, Ω_0, ω_0, f_0, dn_o2, ddn_o6; j2_gc = j2_gc)
 
     # Initialize the structure.
     j2oscd = J2osc_Structure{T}(j2d, epoch, 0, 0, 0, 0, 0, 0, 0)
@@ -75,20 +87,22 @@ end
 """
     j2osc!(j2d::J2osc_Structure{T}, t::Number) where T
 
-Propagate the orbit defined in `j2oscd` (see `J2osc_Structure`) until the time
-`t` [s]. Notice that the values in `j2oscd` will be modified.
+Propagate the orbit defined in `j2oscd` (see [`J2osc_Structure`](@ref)) until
+the time `t` [s].
+
+!!! note
+    The internal values in `j2oscd` will be modified.
 
 # Returns
 
-* The position vector represented in the inertial frame at time `t` [m].
-* The velocity vector represented in the inertial frame at time `t` [m/s]
+- The position vector represented in the inertial frame at time `t` [m].
+- The velocity vector represented in the inertial frame at time `t` [m/s]
 
 # Remarks
 
 The inertial frame in which the output is represented depends on which frame it
 was used to generate the orbit parameters. Notice, however, that the
 perturbation theory requires an inertial frame with true equator.
-
 """
 function j2osc!(j2oscd::J2osc_Structure{T}, t::Number) where T
     # First, we need to propagate the mean elements since they are necessary to
@@ -106,11 +120,11 @@ function j2osc!(j2oscd::J2osc_Structure{T}, t::Number) where T
     ω_k  = j2d.ω_k
     M_k  = j2d.M_k
     f_k  = j2d.f_k
-    p_k  = al_k*R0*(1 - e_k^2)
+    p_k  = al_k * T(R0) * (1 - e_k^2)
     u_k  = ω_k + f_k
 
     # Auxiliary variables to reduce the computational burden.
-    KJ2 = J2*R0*R0
+    KJ2 = J2 * T(R0) * T(R0)
     sin_i_k, cos_i_k = sincos(i_k)
     sin_f_k, cos_f_k = sincos(f_k)
     sin_2u_k, cos_2u_k = sincos(2u_k)
@@ -123,50 +137,50 @@ function j2osc!(j2oscd::J2osc_Structure{T}, t::Number) where T
     e_cos_f_k = e_k * cos_f_k
     e_sin_f_k = e_k * sin_f_k
 
-    aux1 = 3cos(2u_k) + 3e_k*cos_2ω_f_k + e_k*cos_2ω_3f_k
+    aux1 = 3cos(2u_k) + 3e_k * cos_2ω_f_k + e_k * cos_2ω_3f_k
     aux2 = sqrt(1 - e_k²)
     aux3 = 3cos_i_k² - 1
 
     # Compute the short-periodic perturbations considering only the J2
     # gravitational term.
-    δisp_k = +KJ2*sin_i_k*cos_i_k/(4p_k²)*aux1
+    δisp_k = +KJ2 * sin_i_k * cos_i_k / (4p_k²) * aux1
 
-    δpsp_k = +KJ2*sin_i_k²/(2p_k)*aux1
+    δpsp_k = +KJ2 * sin_i_k² / (2p_k) * aux1
 
-    δΩsp_k = -KJ2*cos_i_k/(4p_k²)*(
-        6*(f_k - M_k + e_sin_f_k) - 2sin_2u_k - 3e_k*sin_2ω_f_k - e_k*sin_2ω_3f_k
+    δΩsp_k = -KJ2 * cos_i_k / (4p_k²) * (
+        6 * (f_k - M_k + e_sin_f_k) - 2sin_2u_k - 3e_k * sin_2ω_f_k - e_k * sin_2ω_3f_k
     )
 
-    δrsp_k = -KJ2/(4p_k)*(
-        aux3*( 2*aux2/(1 + e_cos_f_k) + e_cos_f_k/(1 + aux2) ) -
-        sin_i_k²*cos_2u_k
+    δrsp_k = -KJ2 / (4p_k) * (
+        aux3 * (2aux2 / (1 + e_cos_f_k) + e_cos_f_k / (1 + aux2)) - sin_i_k² * cos_2u_k
     )
 
-    δṙsp_k = +KJ2*sqrt(μm)/(4p_k^2.5)*(
-        aux3*e_sin_f_k*( aux2 + (1 + e_cos_f_k)^2/(1 + aux2) ) -
-        sin_i_k²*(1 - e_cos_f_k)^2*sin_2u_k
+    δṙsp_k = +KJ2 * sqrt(μm) / (4p_k^T(2.5)) * (
+        aux3 * e_sin_f_k * (aux2 + (1 + e_cos_f_k)^2 / (1 + aux2)) -
+        sin_i_k² * (1 - e_cos_f_k)^2 * sin_2u_k
     )
 
-    δusp_k = +KJ2/(8p_k²)*(
-        (6 - 30cos_i_k²)*(f_k - M_k) +
-        4e_k*sin_f_k*((1 - 6cos_i_k²) - aux3/(1 + aux2)) -
-        aux3/(1 + aux2)*e_k²*sin(2f_k) +
-        (5cos_i_k² - 2)*2e_k*sin_2ω_f_k +
-        (7cos_i_k² - 1)*sin_2u_k + 2*cos_i_k²*e_k*sin_2ω_3f_k
+    δusp_k = +KJ2 / (8p_k²) * (
+        (6 - 30cos_i_k²) * (f_k - M_k) +
+        4e_k * sin_f_k * ((1 - 6cos_i_k²) - aux3 / (1 + aux2)) -
+        aux3 / (1 + aux2) * e_k² * sin(2f_k) +
+        (5cos_i_k² - 2) * (2e_k) * sin_2ω_f_k +
+        (7cos_i_k² - 1) * sin_2u_k +
+        2 * cos_i_k² * e_k * sin_2ω_3f_k
     )
 
-    r_k = p_k/(1 + e_k*cos(f_k))
-    ṙ_k = sqrt(μm/p_k)*e_k*cos(f_k)
+    r_k = p_k / (1 + e_k * cos_f_k)
+    ṙ_k = sqrt(μm / p_k) * e_k * cos_f_k
 
     r_osc_k = r_k + δrsp_k
     ṙ_osc_k = ṙ_k + δṙsp_k
     p_osc_k = p_k + δpsp_k
 
-    A_k = p_osc_k/r_osc_k - 1
-    B_k = sqrt(p_osc_k/μm)*ṙ_osc_k
+    A_k = p_osc_k / r_osc_k - 1
+    B_k = sqrt(p_osc_k / μm) * ṙ_osc_k
 
     e_osc_k = sqrt(A_k^2 + B_k^2)
-    a_osc_k = p_osc_k/(1-e_osc_k^2)
+    a_osc_k = p_osc_k / (1 - e_osc_k^2)
     i_osc_k = i_k + δisp_k
     Ω_osc_k = Ω_k + δΩsp_k
     u_osc_k = u_k + δusp_k
