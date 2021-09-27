e678e65fb4eb14d9ba4e44e558227ecd3e64d3a3
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-09-27 20:09:51 -0300

    :wrench: Improve J2 osc. prop.

--- src/orbit/propagators/j2osc.jl
+++ src/orbit/propagators/j2osc.jl
@@ -115,12 +115,14 @@ function j2osc!(j2oscd::J2osc_Structure{Tepoch, T}, t::Number) where {Tepoch, T}
     # Obtain the mean elements at this time instant.
     al_k = j2d.al_k
     e_k  = j2d.e_k
+    e_k² = e_k * e_k
     i_k  = j2d.i_k
     Ω_k  = j2d.Ω_k
     ω_k  = j2d.ω_k
     M_k  = j2d.M_k
     f_k  = j2d.f_k
-    p_k  = al_k * R0 * (1 - e_k^2)
+    p_k  = al_k * R0 * (1 - e_k²)
+    p_k² = p_k * p_k
     u_k  = ω_k + f_k
 
     # Auxiliary variables to reduce the computational burden.
@@ -132,8 +134,6 @@ function j2osc!(j2oscd::J2osc_Structure{Tepoch, T}, t::Number) where {Tepoch, T}
     sin_2ω_3f_k, cos_2ω_3f_k = sincos(2ω_k + 3f_k)
     sin_i_k² = sin_i_k * sin_i_k
     cos_i_k² = cos_i_k * cos_i_k
-    p_k² = p_k * p_k
-    e_k² = e_k * e_k
     e_cos_f_k = e_k * cos_f_k
     e_sin_f_k = e_k * sin_f_k
 
@@ -166,7 +166,7 @@ function j2osc!(j2oscd::J2osc_Structure{Tepoch, T}, t::Number) where {Tepoch, T}
         aux3 / (1 + aux2) * e_k² * sin(2f_k) +
         (5cos_i_k² - 2) * (2e_k) * sin_2ω_f_k +
         (7cos_i_k² - 1) * sin_2u_k +
-        2 * cos_i_k² * e_k * sin_2ω_3f_k
+        2cos_i_k² * e_k * sin_2ω_3f_k
     )
 
     r_k = p_k / (1 + e_k * cos_f_k)
@@ -179,14 +179,15 @@ function j2osc!(j2oscd::J2osc_Structure{Tepoch, T}, t::Number) where {Tepoch, T}
     A_k = p_osc_k / r_osc_k - 1
     B_k = sqrt(p_osc_k / μm) * ṙ_osc_k
 
-    e_osc_k = sqrt(A_k^2 + B_k^2)
-    a_osc_k = p_osc_k / (1 - e_osc_k^2)
-    i_osc_k = i_k + δisp_k
-    Ω_osc_k = Ω_k + δΩsp_k
-    u_osc_k = u_k + δusp_k
-    f_osc_k = atan(B_k, A_k)
-    ω_osc_k = u_osc_k - f_osc_k
-    M_osc_k = f_to_M(e_osc_k, f_osc_k)
+    e_osc_k² = A_k^2 + B_k^2
+    e_osc_k  = sqrt(e_osc_k²)
+    a_osc_k  = p_osc_k / (1 - e_osc_k²)
+    i_osc_k  = i_k + δisp_k
+    Ω_osc_k  = Ω_k + δΩsp_k
+    u_osc_k  = u_k + δusp_k
+    f_osc_k  = atan(B_k, A_k)
+    ω_osc_k  = u_osc_k - f_osc_k
+    M_osc_k  = f_to_M(e_osc_k, f_osc_k)
 
     # Compute the position and velocity considering the osculating elements.
     r_i_k, v_i_k = kepler_to_rv(a_osc_k, e_osc_k, i_osc_k, Ω_osc_k, ω_osc_k, f_osc_k)
