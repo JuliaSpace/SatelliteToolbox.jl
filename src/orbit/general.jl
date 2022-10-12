4d5f0b96c6ca130663e7bd2caa17b60fc076e0c4
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-10-12 11:43:16 -0300

    :art: Apply BlueStyle to s/o/general.jl

--- src/orbit/general.jl
+++ src/orbit/general.jl
@@ -1,10 +1,11 @@
-#== # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Description
+# ==============================================================================
 #
 #   Functions to compute general values related to the orbit.
 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ==#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 export @check_orbit
 export angvel, angvel_to_a, dargp, draan, period
@@ -20,18 +21,17 @@ Verify if the orbit with semi-major axis `a` [m] and eccentricity `e` is valid.
 This macro throws an exception if the orbit is not valid.
 
 Return `true` is the orbit is valid, and `false` otherwise.
-
 """
 macro check_orbit(a, e)
     quote
         # Check if the arguments are valid.
         if $(esc(a)) < 0
             false
-        elseif !( 0 <= $(esc(e)) < 1 )
+        elseif !(0 <= $(esc(e)) < 1)
             false
         else
             # Compute the orbit perigee and check if it is inside Earth.
-            perigee = $(esc(a))*(1-$(esc(e)))
+            perigee = $(esc(a)) * (1 - $(esc(e)))
 
             if perigee < R0
                 false
@@ -82,7 +82,7 @@ If `pert` is omitted, then it defaults to `:J2`.
         p = T(a) * aux
 
         # Orbit period considering the perturbations (up to J2).
-        return n0 + 3T(R0)^2 * T(J2) / (4p^2) * n0 *(sqrt(aux) * (3cos_i² - 1) + (5cos_i² - 1))
+        return n0 + 3T(R0)^2 * T(J2) / (4p^2) * n0 * (sqrt(aux) * (3cos_i² - 1) + (5cos_i² - 1))
 
     # Perturbation computed using perturbations terms J2, J4, and J2².
     elseif pert == :J4
@@ -110,7 +110,7 @@ If `pert` is omitted, then it defaults to `:J2`.
                T(15 / 128) * n0 * T(J4)   / p⁴ * (64 + 72e² - (248 + 252e²) * sin_i² + (196 + 189e²) * sin_i⁴)
 
         δM_0 = T( 3 /   4) * n0 * T(J2  ) / p² * saux * (2 - 3sin_i²) +
-               T( 3 / 512) * n0 * T(J2^2) / p⁴ / saux * (320e² - 280e⁴ + (1600 - 1568e² + 328e⁴)*sin_i² + (-2096 + 1072e² +  79e⁴)*sin_i⁴) -
+               T( 3 / 512) * n0 * T(J2^2) / p⁴ / saux * (320e² - 280e⁴ + (1600 - 1568e² + 328e⁴) * sin_i² + (-2096 + 1072e² +  79e⁴) * sin_i⁴) -
                T(45 / 128) * n0 * T(J4  ) / p⁴ * saux * e² * (-8 + 40sin_i - 35sin_i²)
 
         return n0 + δω + δM_0
@@ -137,24 +137,29 @@ time between two consecutive passages by the ascending node.
 
 `pert` can be:
 
-* `:J0`: Consider a Keplerian orbit.
-* `:J2`: Consider the perturbation terms up to J2.
-* `:J4`: Consider the perturbation terms J2, J4, and J2².
+- `:J0`: Consider a Keplerian orbit.
+- `:J2`: Consider the perturbation terms up to J2.
+- `:J4`: Consider the perturbation terms J2, J4, and J2².
 
 If `pert` is omitted, then it defaults to `:J2`.
 
 # Keyword
 
-* `μ`: Standard gravitational parameter for Earth [m^3/s^2].
-       (**Default** = `m0`)
-* `max_iter`: Maximum number of iterations allowed in the Newton-Raphson
-              algorithm. (**Default** = 20)
-* `tol`: Tolerance to stop the Newton-Raphson algorithm. (**Default** = 1e-10)
-
+- `μ`: Standard gravitational parameter for Earth [m^3/s^2].
+    (**Default** = `m0`)
+- `max_iter`: Maximum number of iterations allowed in the Newton-Raphson
+    algorithm. (**Default** = 20)
+- `tol`: Tolerance to stop the Newton-Raphson algorithm. (**Default** = 1e-10)
 """
-function angvel_to_a(n::Number, e::Number, i::Number, pert::Symbol = :J2;
-                     μ::Number = m0, max_iter::Int = 20, tol::Number = 1e-10)
-
+function angvel_to_a(
+    n::Number,
+    e::Number,
+    i::Number,
+    pert::Symbol = :J2;
+    μ::Number = m0,
+    max_iter::Int = 20,
+    tol::Number = 1e-10
+)
     if pert == :J0
 
         a = (μ/n^2)^(1/3)
@@ -182,47 +187,47 @@ function angvel_to_a(n::Number, e::Number, i::Number, pert::Symbol = :J2;
         # semi-major axis, i.e. `a/R0`.
 
         # Auxiliary variables to solve for the semi-major axis.
-        sqrt_μ = sqrt(μ/R0^3)
+        sqrt_μ = sqrt(μ / R0^3)
         cos_i  = cos(i)
-        K      = 3/4*J2*( sqrt(1-e^2)*(3cos_i^2-1) + (5cos_i^2-1) )/(1-e^2)^2
+        K      = 3 / 4 * J2 * (sqrt(1 - e^2) * (3cos_i^2 - 1) + (5cos_i^2 - 1)) / (1 - e^2)^2
 
         # Initial guess using a non-perturbed orbit.
-        a = (μ/n^2)^(1/3)/R0
+        a = (μ / n^2)^(1 / 3) / R0
 
         # Newton-Raphson algorithm.
         #
         # Notice that we will allow, at most, 20 iterations.
         for k = 1:20
             # Auxiliary variables.
-            ap3o2  = a^(3/2)
-            ap5o2  = ap3o2*a # -> a^(5/2)
-            ap7o2  = ap5o2*a # -> a^(7/2)
-            ap11o2 = ap7o2*a # -> a^(11/2)
+            ap3o2  = a^(3 / 2)
+            ap5o2  = ap3o2 * a # -> a^(5/2)
+            ap7o2  = ap5o2 * a # -> a^(7/2)
+            ap11o2 = ap7o2 * a # -> a^(11/2)
 
             # Compute the residue.
-            res = n - sqrt_μ/ap3o2 - K*sqrt_μ/ap7o2
+            res = n - sqrt_μ / ap3o2 - K * sqrt_μ / ap7o2
 
             # Compute the Jacobian of the function.
-            df = +3/2*sqrt_μ/ap5o2 + 7/2*K*sqrt_μ/ap11o2
+            df = +3 / 2 * sqrt_μ / ap5o2 + 7 / 2 * K * sqrt_μ / ap11o2
 
             # Compute the new estimate.
-            a = a - res/df
+            a = a - res / df
 
             (abs(res) < tol) && break
         end
 
-        return a*R0
+        return a * R0
 
     elseif pert == :J4
 
         # Auxiliary variables
-        sqrt_μ = sqrt(μ/R0^3)
+        sqrt_μ = sqrt(μ / R0^3)
         sin_i  = sin(i)
         sin_i² = sin_i^2
         sin_i⁴ = sin_i^3
         e²     = e^2
         e⁴     = e^4
-        aux    = 1-e²
+        aux    = 1 - e²
         saux   = sqrt(aux)
 
         # Get the semi-major axis using J4 perturbation theory [er].
@@ -243,44 +248,51 @@ function angvel_to_a(n::Number, e::Number, i::Number, pert::Symbol = :J2;
         # To improve algorithm stability, we will compute the normalized
         # semi-major axis, i.e. `a/R0`.
 
-        K₁ = 3/4*J2/aux^2*( (4 - 5sin_i²) + sqrt(1-e²)*(2 - 3sin_i²) )
+        K₁ = 3 / 4 * J2 / aux^2 * ((4 - 5sin_i²) + sqrt(1 - e²) * (2 - 3sin_i²))
 
-        K₂ = 3/512*J2^2/aux^4*(224e² + (3040 - 144e²)*sin_i² - (3560 + 180e²)*sin_i⁴ +
-                               1/saux*((         320e² - 280e⁴) +
-                                       ( 1600 - 1568e² + 328e⁴)*sin_i² +
-                                       (-2096 + 1072e² +  79e⁴)*sin_i⁴))
+        K₂ = 3 / 512 * J2^2 / aux^4 * (
+            224e² + (3040 - 144e²) * sin_i² - (3560 + 180e²) * sin_i⁴ + 1 / saux * (
+                (         320e² - 280e⁴) +
+                ( 1600 - 1568e² + 328e⁴) * sin_i² +
+                (-2096 + 1072e² +  79e⁴) * sin_i⁴
+            )
+        )
 
-        K₃ = 15/128*J4/aux^4*(64 + 72e² - (248 + 252e²)*sin_i² + (196 + 189e²)*sin_i⁴ +
-                              e²/saux*(-8 + 40sin_i - 35sin_i²))
+        K₃ = 15 / 128 * J4 / aux^4 * (
+            64 + 72e² -
+            (248 + 252e²) * sin_i² +
+            (196 + 189e²) * sin_i⁴ +
+            e² / saux * (-8 + 40sin_i - 35sin_i²)
+        )
 
         # Initial guess using a non-perturbed orbit.
-        a = (μ/n^2)^(1/3)/R0
+        a = (μ / n^2)^(1 / 3) / R0
 
         # Newton-Raphson algorithm.
         #
         # Notice that we will allow, at most, 20 iterations.
         for k = 1:20
             # Auxiliary variables.
-            ap3o2  = a^(3/2)
-            ap5o2  =  ap3o2*a  # -> a^(5/2)
-            ap7o2  =  ap5o2*a  # -> a^(7/2)
-            ap9o2  =  ap7o2*a  # -> a^(9/2)
-            ap11o2 =  ap9o2*a  # -> a^(11/2)
-            ap13o2 = ap11o2*a  # -> a^(13/2)
+            ap3o2  = a^(3 / 2)
+            ap5o2  =  ap3o2 * a  # -> a^(5/2)
+            ap7o2  =  ap5o2 * a  # -> a^(7/2)
+            ap9o2  =  ap7o2 * a  # -> a^(9/2)
+            ap11o2 =  ap9o2 * a  # -> a^(11/2)
+            ap13o2 = ap11o2 * a  # -> a^(13/2)
 
             # Compute the residue.
-            res = n - sqrt_μ*(1/ap3o2 + K₁/ap7o2 + (K₂ + K₃)/ap11o2)
+            res = n - sqrt_μ * (1 / ap3o2 + K₁ / ap7o2 + (K₂ + K₃) / ap11o2)
 
             # Compute the Jacobian of the function.
-            df = sqrt_μ/2*( 3ap5o2 + 7K₁/ap9o2 + 11*(K₂ + K₃)/ap13o2)
+            df = sqrt_μ / 2 * (3ap5o2 + 7K₁ / ap9o2 + 11 * (K₂ + K₃) / ap13o2)
 
             # Compute the new estimate.
-            a = a - res/df
+            a = a - res / df
 
             (abs(res) < tol) && break
         end
 
-        return a*R0
+        return a * R0
     else
         throw(ArgumentError("The perturbation parameter $pert is not defined."))
     end
@@ -297,12 +309,11 @@ specified by `orb` (see [`Orbit`](@ref)).
 
 `pert` can be:
 
-* `:J0`: Consider a Keplerian orbit.
-* `:J2`: Consider the perturbation terms up to J2.
-* `:J4`: Consider the perturbation terms J2, J4, and J2².
+- `:J0`: Consider a Keplerian orbit.
+- `:J2`: Consider the perturbation terms up to J2.
+- `:J4`: Consider the perturbation terms J2, J4, and J2².
 
 If `pert` is omitted, then it defaults to `:J2`.
-
 """
 @inline function dargp(a::Number, e::Number, i::Number, pert::Symbol = :J2)
     # Perturbation computed using a Keplerian orbit.
@@ -311,13 +322,13 @@ If `pert` is omitted, then it defaults to `:J2`.
     # Perturbation computed using perturbations terms up to J2.
     elseif pert == :J2
         # Semi-lactum rectum.
-        p = a*(1-e^2)
+        p = a * (1 - e^2)
 
         # Unperturbed orbit period.
         n0 = angvel(a, e, i, :J0)
 
         # Perturbation of the argument of perigee.
-        return 3R0^2*J2/(4p^2)*n0*(5cos(i)^2-1)
+        return 3R0^2 * J2 / (4p^2) * n0 * (5cos(i)^2 - 1)
 
     elseif pert == :J4
         # Auxiliary variables
@@ -326,9 +337,9 @@ If `pert` is omitted, then it defaults to `:J2`.
         sin_i  = sin(i)
         sin_i² = sin_i^2
         sin_i⁴ = sin_i^4
-        aux    = (1-e²)
+        aux    = (1 - e²)
         saux   = sqrt(aux)
-        p      = (a/R0)*aux
+        p      = (a / R0) * aux
         p²     = p^2
         p⁴     = p^4
 
@@ -336,9 +347,9 @@ If `pert` is omitted, then it defaults to `:J2`.
         n0 = angvel(a, e, i, :J0)
 
         # Perturbation of the argument of perigee.
-        δω =  3/  4*n0*J2  /p²*(4 - 5sin_i²) +
-              9/384*n0*J2^2/p⁴*(     56e² + (760 -  36e²)*sin_i² - (890 +  45*e²)*sin_i⁴) -
-             15/128*n0*J4  /p⁴*(64 + 72e² - (248 + 252e²)*sin_i² + (196 + 189*e²)*sin_i⁴)
+        δω =  3 /  4  * n0 * J2   / p² * (4 - 5sin_i²) +
+              9 / 384 * n0 * J2^2 / p⁴ * (     56e² + (760 -  36e²) * sin_i² - (890 +  45e²) * sin_i⁴) -
+             15 / 128 * n0 * J4   / p⁴ * (64 + 72e² - (248 + 252e²) * sin_i² + (196 + 189e²) * sin_i⁴)
 
         return δω
     else
@@ -363,12 +374,11 @@ can also be specified by `orb` (see [`Orbit`](@ref)).
 
 `pert` can be:
 
-* `:J0`: Consider a Keplerian orbit.
-* `:J2`: Consider the perturbation terms up to J2.
-* `:J4`: Consider the perturbation terms J2, J4, and J2².
+- `:J0`: Consider a Keplerian orbit.
+- `:J2`: Consider the perturbation terms up to J2.
+- `:J4`: Consider the perturbation terms J2, J4, and J2².
 
 If `pert` is omitted, then it defaults to `:J2`.
-
 """
 @inline function draan(a::Number, e::Number, i::Number, pert::Symbol = :J2)
     # Perturbation computed using a Keplerian orbit.
@@ -377,13 +387,13 @@ If `pert` is omitted, then it defaults to `:J2`.
     # Perturbation computed using perturbations terms up to J2.
     elseif pert == :J2
         # Semi-lactum rectum.
-        p = a*(1-e^2)
+        p = a * (1 - e^2)
 
         # Unperturbed orbit period.
         n0 = angvel(a, e, i, :J0)
 
         # Perturbation of the right ascension of the ascending node.
-        return -3/2*R0^2/(p^2)*n0*J2*cos(i)
+        return -3 / 2 * R0^2 / (p^2) * n0 * J2 * cos(i)
 
     # Perturbation computed using perturbation terms J2, J4, and J2².
     elseif pert == :J4
@@ -391,7 +401,7 @@ If `pert` is omitted, then it defaults to `:J2`.
         sin_i, cos_i = sincos(i)
         sin_i²       = sin_i^2
         e²           = e^2
-        p            = (a/R0)*(1-e²)
+        p            = (a / R0)*(1 - e²)
         p²           = p^2
         p⁴           = p^4
 
@@ -399,9 +409,9 @@ If `pert` is omitted, then it defaults to `:J2`.
         n0 = angvel(a, e, i, :J0)
 
         # Perturbation of the right ascension of the ascending node.
-        δΩ = -3/2 *n0*J2  /p²*cos_i +
-              3/32*n0*J2^2/p⁴*cos_i*(12 -  4e² - (80 +  5e²)*sin_i²) +
-             15/32*n0*J4  /p⁴*cos_i*( 8 + 12e² - (14 + 21e²)*sin_i²)
+        δΩ = -3 / 2  * n0 * J2   / p² * cos_i +
+              3 / 32 * n0 * J2^2 / p⁴ * cos_i * (12 -  4e² - (80 +  5e²) * sin_i²) +
+             15 / 32 * n0 * J4   / p⁴ * cos_i * ( 8 + 12e² - (14 + 21e²) * sin_i²)
 
         return δΩ
     else
@@ -425,12 +435,11 @@ specified by the symbol `pert`. The orbit can also be specified by `orb` (see
 
 pert` can be:
 
-* `:J0`: Consider a Keplerian orbit.
-* `:J2`: Consider the perturbation terms up to J2.
-* `:J4`: Consider the perturbation terms J2, J4, and J2².
+- `:J0`: Consider a Keplerian orbit.
+- `:J2`: Consider the perturbation terms up to J2.
+- `:J4`: Consider the perturbation terms J2, J4, and J2².
 
 If `pert` is omitted, then it defaults to `:J2`.
-
 """
 @inline function period(a::Number, e::Number, i::Number, pert::Symbol = :J2)
     n = angvel(a, e, i, pert)
