2b4e6d4141bfbecd420e5f6445528fff8c3799ee
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-10-12 14:21:54 -0300

    :bug: Add missing deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -11,8 +11,11 @@
 # ==============================================================================
 
 @deprecate angvel(a, e, i, pert) angvel(a, e, i; pert = pert)
+@deprecate angvel(orb, pert) angvel(orb; pert = pert)
 @deprecate angvel_to_a(a, e, i, pert; kwargs...) angvel_to_a(a, e, i; pert = pert, kwargs...)
+@deprecate angvel_to_a(orb, pert; kwargs...) angvel_to_a(orb; pert = pert, kwargs...)
 @deprecate dargp(a, e, i, pert) dargp(a, e, i; pert = pert)
+@deprecate dargp(orb, pert) dargp(orb; pert = pert)
 
 # Deprecations introduced in SatelliteToolbox v0.10
 # ==============================================================================
